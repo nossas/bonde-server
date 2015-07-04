169f2e718a7aa2a3ba4453860ff9e64293181c85
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-07-03 22:02:06 -0300

    Fix devise_token_auth routes

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -18,5 +18,5 @@ DeviseTokenAuth.setup do |config|
   # This route will be the prefix for all oauth2 redirect callbacks. For
   # example, using the default '/omniauth', the github oauth2 provider will
   # redirect successful authentications to '/omniauth/github/callback'
-  config.omniauth_prefix = "/v1/omniauth"
+  # config.omniauth_prefix = "/v1/omniauth"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :v1 do
-    mount_devise_token_auth_for 'User', at: '/auth'
-  end
+  mount_devise_token_auth_for 'User', at: '/auth'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
