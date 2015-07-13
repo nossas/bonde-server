682cec562c811a7e8337f41de5443f22a54ff1c6
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-07-13 17:31:16 -0300

    [#98297248] add mobilization model

---
+++ app/models/mobilization.rb
@@ -0,0 +1,3 @@
+class Mobilization < ActiveRecord::Base
+  validates :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150703175403) do
+ActiveRecord::Schema.define(version: 20150713202918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "mobilizations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",                            null: false
