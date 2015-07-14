60c618cab673220d7c6e43e4279a7892413ecde9
Daniel Weinmann <danielweinmann@gmail.com>
2015-07-14 17:15:26 -0300

    Refactors specs to use should instead of is_expected

--- spec/models/block_spec.rb
+++ spec/models/block_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Block, type: :model do
-  describe "asociations" do
-    it{ is_expected.to belong_to :mobilization }
-  end
-
-  describe "validations" do
-    it{ is_expected.to validate_presence_of :mobilization_id }
-  end
+  it { should belong_to :mobilization }
+  it { should validate_presence_of :mobilization_id }
 end
--- spec/models/mobilization_spec.rb
+++ spec/models/mobilization_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Mobilization, type: :model do
-  describe "asociations" do
-    it{ is_expected.to belong_to :user }
-    it{ is_expected.to have_many :blocks }
-  end
-
-  describe "validations" do
-    it{ is_expected.to validate_presence_of :user_id }
-  end
+  it { should belong_to :user }
+  it { should have_many :blocks }
+  it { should validate_presence_of :user_id }
 end
