0734d896f999a1b1747d6cffa493859f5f3ff38d
Daniel Weinmann <danielweinmann@gmail.com>
2015-07-14 17:56:29 -0300

    Adds associations between mobilizations and blocks and widgets

--- app/models/mobilization.rb
+++ app/models/mobilization.rb
@@ -2,4 +2,5 @@ class Mobilization < ActiveRecord::Base
   validates :name, :user_id, presence: true
   belongs_to :user
   has_many :blocks
+  has_many :widgets, through: :blocks
 end
--- spec/models/block_spec.rb
+++ spec/models/block_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Block, type: :model do
   it { should belong_to :mobilization }
+  it { should have_many :widgets }
   it { should validate_presence_of :mobilization_id }
 end
