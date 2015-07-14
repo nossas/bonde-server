1af434a47a36b4f4eb2c94804431c6b52a2058d7
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-07-14 11:16:40 -0300

    [#98297248] refactor mobilizations controller

--- spec/controllers/mobilizations_controller_spec.rb
+++ spec/controllers/mobilizations_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe MobilizationsController, type: :controller do
   describe "GET #index" do
     before do
-      @user1 = User.create password: "12345678", uid: "nicolas1@trashmail.com", provider: "email", email: "nicolas1@trashmail.com"
-      @user2 = User.create password: "12345678", uid: "nicolas2@trashmail.com", provider: "email", email: "nicolas2@trashmail.com"
-      @mob1 = Mobilization.create name: "My Mobilization 1", user: @user1
-      @mob2 = Mobilization.create name: "My Mobilization 2", user: @user2
+      @user1 = User.make!
+      @user2 = User.make!
+      @mob1 = Mobilization.make! user: @user1
+      @mob2 = Mobilization.make! user: @user2
     end
 
     it "should return all mobilizations" do
@@ -18,7 +18,7 @@ RSpec.describe MobilizationsController, type: :controller do
 
     it "should return mobilizations by user" do
       get :index, user_id: @user1.id
-      
+
       expect(response.body).to include(@mob1.name)
       expect(response.body).to_not include(@mob2.name)
     end
