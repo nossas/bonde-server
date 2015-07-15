113d51fdfdb410d23926223a6043e655790638d1
Daniel Weinmann <danielweinmann@gmail.com>
2015-07-14 21:17:09 -0300

    Adds respond_to :json to Mobilizations::WidgetsController

--- app/controllers/mobilizations/widgets_controller.rb
+++ app/controllers/mobilizations/widgets_controller.rb
@@ -1,4 +1,6 @@
 class Mobilizations::WidgetsController < ApplicationController
+  respond_to :json
+
   def index
     @widgets = Widget.joins(:block).where(blocks: {mobilization_id: params[:mobilization_id]})
     render json: @widgets
