68e3378d40a9126c1cb3940447e92af904924ca0
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-07-25 12:41:19 -0300

    [#98297792] add user_not_authorized method to rescue from pundit not authorized error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
   include Pundit
-  
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
+  end
+
+  def user_not_authorized
+    render nothing: true, status: :unauthorized
   end
 end
