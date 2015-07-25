ddc8575f4613e87ad4cdf404763da2a282c5a9ab
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-07-25 10:38:53 -0300

    [#98297792] add before filter to set user by token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
