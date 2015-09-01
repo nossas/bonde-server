16c0bcd22638bfc23d45e45fec0d6e55b4348e5b
Josias Schneider <josias.ds@gmail.com>
2015-09-01 17:55:02 -0300

    [#98310562] add header_font and body_font to mobilization's permitted params

--- app/policies/mobilization_policy.rb
+++ app/policies/mobilization_policy.rb
@@ -1,7 +1,7 @@
 class MobilizationPolicy < ApplicationPolicy
   def permitted_attributes
     if create? || update?
-      [:name, :color_scheme, :google_analytics_code, :goal]
+      [:name, :color_scheme, :google_analytics_code, :goal, :header_font, :body_font]
     else
       []
     end
