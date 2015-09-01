9ec82f163e079d9e95975e0ea3ea3a1a8bc39aa4
Daniel Weinmann <danielweinmann@gmail.com>
2015-09-01 04:36:46 -0300

    Fixes specs

--- app/controllers/mobilizations/form_entries_controller.rb
+++ app/controllers/mobilizations/form_entries_controller.rb
@@ -14,11 +14,11 @@ class Mobilizations::FormEntriesController < ApplicationController
     fields.each do |field|
       if !email && field['kind'] == 'email'
         email = field['value']
-      elsif !first_name && ['nome'].include?(field['label'].downcase)
+      elsif field['label'] && !first_name && ['nome'].include?(field['label'].downcase)
         first_name = field['value']
-      elsif !last_name && ['sobrenome', 'sobre-nome', 'sobre nome'].include?(field['label'].downcase)
+      elsif field['label'] && !last_name && ['sobrenome', 'sobre-nome', 'sobre nome'].include?(field['label'].downcase)
         last_name = field['value']
-      elsif !phone && ['telefone', 'fone', 'celular'].include?(field['label'].downcase)
+      elsif field['label'] && !phone && ['telefone', 'fone', 'celular'].include?(field['label'].downcase)
         phone = field['value']
       end
     end
@@ -26,21 +26,24 @@ class Mobilizations::FormEntriesController < ApplicationController
     merge_vars[:FNAME] = first_name if first_name
     merge_vars[:LNAME] = last_name if last_name
     merge_vars[:PHONE] = phone if phone
-    mailchimp = Gibbon::API.new
-    mailchimp.lists.subscribe({id: ENV['MAILCHIMP_LIST_ID'], email: {email: email}, merge_vars: merge_vars, double_optin: false, update_existing: true})
-    mobilization = @form_entry.widget.mobilization
-    segments = mailchimp.lists.static_segments({id: ENV['MAILCHIMP_LIST_ID']})
-    segments.each do |segment|
-      if /\A##{mobilization.id} - .+/.match(segment["name"])
-        @segment = segment
-        break
+    begin
+      mailchimp = Gibbon::API.new
+      mailchimp.lists.subscribe({id: ENV['MAILCHIMP_LIST_ID'], email: {email: email}, merge_vars: merge_vars, double_optin: false, update_existing: true})
+      mobilization = @form_entry.widget.mobilization
+      segments = mailchimp.lists.static_segments({id: ENV['MAILCHIMP_LIST_ID']})
+      segments.each do |segment|
+        if /\A##{mobilization.id} - .+/.match(segment["name"])
+          @segment = segment
+          break
+        end
       end
-    end
-    unless @segment
-      @segment = mailchimp.lists.static_segment_add({id: ENV['MAILCHIMP_LIST_ID'], name: "##{mobilization.id} - #{mobilization.name}"})
-    end
-    if @segment
-      mailchimp.lists.static_segment_members_add({id: ENV['MAILCHIMP_LIST_ID'], seg_id: @segment["id"], batch: [{email: email}]})
+      unless @segment
+        @segment = mailchimp.lists.static_segment_add({id: ENV['MAILCHIMP_LIST_ID'], name: "##{mobilization.id} - #{mobilization.name}"})
+      end
+      if @segment
+        mailchimp.lists.static_segment_members_add({id: ENV['MAILCHIMP_LIST_ID'], seg_id: @segment["id"], batch: [{email: email}]})
+      end
+    rescue
     end
     render json: @form_entry
   end
--- spec/controllers/mobilizations/form_entries_controller_spec.rb
+++ spec/controllers/mobilizations/form_entries_controller_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe Mobilizations::FormEntriesController, type: :controller do
     it "should create with JSON format and parameters" do
       widget = Widget.make!
       expect(widget.form_entries.count).to eq(0)
-      post :create, mobilization_id: widget.mobilization.id, format: :json, form_entry: { widget_id: widget.id, fields: 'ABC' }
+      post :create, mobilization_id: widget.mobilization.id, format: :json, form_entry: { widget_id: widget.id, fields: [{email: 'foo@bar.com'}].to_json }
       expect(widget.form_entries.count).to eq(1)
       form_entry = widget.form_entries.first
       expect(response.body).to include(form_entry.to_json)
       expect(form_entry.widget_id).to eq(widget.id)
-      expect(form_entry.fields).to eq('ABC')
+      expect(form_entry.fields).to eq([{email: 'foo@bar.com'}].to_json)
     end
   end
 end
