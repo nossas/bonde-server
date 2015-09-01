ec990e373a8eb205f1f4fa5aa452292cd1b7f29f
Daniel Weinmann <danielweinmann@gmail.com>
2015-09-01 04:21:17 -0300

    Integrates form entries with mailchimp

--- app/controllers/mobilizations/form_entries_controller.rb
+++ app/controllers/mobilizations/form_entries_controller.rb
@@ -5,7 +5,43 @@ class Mobilizations::FormEntriesController < ApplicationController
   def create
     @form_entry = FormEntry.new(form_entry_params)
     authorize @form_entry
-    @form_entry.save
+    @form_entry.save!
+    fields = JSON.parse(@form_entry.fields)
+    email = nil
+    first_name = nil
+    last_name = nil
+    phone = nil
+    fields.each do |field|
+      if !email && field['kind'] == 'email'
+        email = field['value']
+      elsif !first_name && ['nome'].include?(field['label'].downcase)
+        first_name = field['value']
+      elsif !last_name && ['sobrenome', 'sobre-nome', 'sobre nome'].include?(field['label'].downcase)
+        last_name = field['value']
+      elsif !phone && ['telefone', 'fone', 'celular'].include?(field['label'].downcase)
+        phone = field['value']
+      end
+    end
+    merge_vars = {}
+    merge_vars[:FNAME] = first_name if first_name
+    merge_vars[:LNAME] = last_name if last_name
+    merge_vars[:PHONE] = phone if phone
+    mailchimp = Gibbon::API.new
+    mailchimp.lists.subscribe({id: ENV['MAILCHIMP_LIST_ID'], email: {email: email}, merge_vars: merge_vars, double_optin: false, update_existing: true})
+    mobilization = @form_entry.widget.mobilization
+    segments = mailchimp.lists.static_segments({id: ENV['MAILCHIMP_LIST_ID']})
+    segments.each do |segment|
+      if /\A##{mobilization.id} - .+/.match(segment["name"])
+        @segment = segment
+        break
+      end
+    end
+    unless @segment
+      @segment = mailchimp.lists.static_segment_add({id: ENV['MAILCHIMP_LIST_ID'], name: "##{mobilization.id} - #{mobilization.name}"})
+    end
+    if @segment
+      mailchimp.lists.static_segment_members_add({id: ENV['MAILCHIMP_LIST_ID'], seg_id: @segment["id"], batch: [{email: email}]})
+    end
     render json: @form_entry
   end
 
