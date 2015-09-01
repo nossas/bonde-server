4b2b2cc7f6814edebb18c355afdbe4d7baf103fd
Daniel Weinmann <danielweinmann@gmail.com>
2015-09-01 04:40:38 -0300

    Only integrates with mailchimp if form entry has an email

--- app/controllers/mobilizations/form_entries_controller.rb
+++ app/controllers/mobilizations/form_entries_controller.rb
@@ -26,24 +26,26 @@ class Mobilizations::FormEntriesController < ApplicationController
     merge_vars[:FNAME] = first_name if first_name
     merge_vars[:LNAME] = last_name if last_name
     merge_vars[:PHONE] = phone if phone
-    begin
-      mailchimp = Gibbon::API.new
-      mailchimp.lists.subscribe({id: ENV['MAILCHIMP_LIST_ID'], email: {email: email}, merge_vars: merge_vars, double_optin: false, update_existing: true})
-      mobilization = @form_entry.widget.mobilization
-      segments = mailchimp.lists.static_segments({id: ENV['MAILCHIMP_LIST_ID']})
-      segments.each do |segment|
-        if /\A##{mobilization.id} - .+/.match(segment["name"])
-          @segment = segment
-          break
+    if email
+      begin
+        mailchimp = Gibbon::API.new
+        mailchimp.lists.subscribe({id: ENV['MAILCHIMP_LIST_ID'], email: {email: email}, merge_vars: merge_vars, double_optin: false, update_existing: true})
+        mobilization = @form_entry.widget.mobilization
+        segments = mailchimp.lists.static_segments({id: ENV['MAILCHIMP_LIST_ID']})
+        segments.each do |segment|
+          if /\A##{mobilization.id} - .+/.match(segment["name"])
+            @segment = segment
+            break
+          end
         end
+        unless @segment
+          @segment = mailchimp.lists.static_segment_add({id: ENV['MAILCHIMP_LIST_ID'], name: "##{mobilization.id} - #{mobilization.name}"})
+        end
+        if @segment
+          mailchimp.lists.static_segment_members_add({id: ENV['MAILCHIMP_LIST_ID'], seg_id: @segment["id"], batch: [{email: email}]})
+        end
+      rescue
       end
-      unless @segment
-        @segment = mailchimp.lists.static_segment_add({id: ENV['MAILCHIMP_LIST_ID'], name: "##{mobilization.id} - #{mobilization.name}"})
-      end
-      if @segment
-        mailchimp.lists.static_segment_members_add({id: ENV['MAILCHIMP_LIST_ID'], seg_id: @segment["id"], batch: [{email: email}]})
-      end
-    rescue
     end
     render json: @form_entry
   end
