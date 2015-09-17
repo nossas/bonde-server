66ca14fc0615eaf3e7a6729cc2986d8f2fc3125e
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-17 15:56:22 -0300

    [#100327124] fix Sendgrid config

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SENDGRID_USERNAME"],
-  :password => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PASSWORD"],
   :domain => 'hub-api.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
