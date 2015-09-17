2aca5eb2bdd8f4f3dbdb223469ce6b02aca016be
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-17 11:43:05 -0300

    Add url method to Mobilization and include Shareable to it

--- app/models/mobilization.rb
+++ app/models/mobilization.rb
@@ -1,6 +1,12 @@
 class Mobilization < ActiveRecord::Base
+  include Shareable
+
   validates :name, :user_id, :goal, presence: true
   belongs_to :user
   has_many :blocks
   has_many :widgets, through: :blocks
+
+  def url
+    "#{ENV["CLIENT_URL"]}/mobilizations/#{self.id}"
+  end
 end
