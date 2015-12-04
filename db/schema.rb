426b7cbf285600677295cd48dc6e63aa91d694b8
nicolasiensen <nicolas.iensen@gmail.com>
2015-12-04 18:27:45 -0200

    [#109181210] add organization_id to mobilization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203190156) do
+ActiveRecord::Schema.define(version: 20151204202424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20151203190156) do
     t.string   "slug"
     t.string   "custom_domain"
     t.string   "twitter_share_text",         limit: 140
+    t.integer  "organization_id"
   end
 
   create_table "organizations", force: :cascade do |t|
