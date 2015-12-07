561c007c86af88f12930fa2b9caf1c2b461dd075
nicolasiensen <nicolas.iensen@gmail.com>
2015-12-07 17:20:11 -0200

    [#109181210] fix test

--- spec/models/mobilization_spec.rb
+++ spec/models/mobilization_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe Mobilization, type: :model do
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :name }
   it { should validate_presence_of :goal }
-  it { should validate_presence_of :organization_id }
   it { should validate_length_of :twitter_share_text }
 
   before { @organization = Organization.make! }
