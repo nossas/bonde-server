edb98461e2beb937a2b4db722125dbc83471522a
Leticia Figueira <jlcfigueira@gmail.com>
2016-02-17 16:35:01 -0200

    Disables WebMock when using CodeClimate coverage tool 😠

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,4 +24,8 @@ RSpec.configure do |config|
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
+
+  config.after(:suite) do
+    WebMock.disable_net_connect!(:allow => 'codeclimate.com')
+  end
 end
