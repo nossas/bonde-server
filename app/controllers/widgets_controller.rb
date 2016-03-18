ec530fed548b81568397d6d7db8e49da41831f7b
Leticia Figueira <jlcfigueira@gmail.com>
2016-03-18 14:21:07 -0300

    Returns same type of widgets collection

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -3,9 +3,10 @@ class WidgetsController < ApplicationController
   after_action :verify_authorized, except: %i[index]
   after_action :verify_policy_scoped, only: %i[index]
 
+  # FIXME efficiency
   def index
     mobilization = policy_scope(Mobilization).filter(params.slice(:custom_domain, :slug))
-    widgets = mobilization.present? ? mobilization.widgets.order(:id) : []
+    widgets = mobilization.present? ? mobilization.widgets.order(:id) : Widget.none
     render json: widgets
   end
 end
