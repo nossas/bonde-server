fb16103c5c15dcf4e3da6b363ed2f371ee69166c
Lucas Pirola <lucaspirola@gmail.com>
2016-04-06 18:14:07 -0300

    [#116588043] Add new options to donation at widget settings

--- app/policies/widget_policy.rb
+++ app/policies/widget_policy.rb
@@ -1,7 +1,24 @@
 class WidgetPolicy < ApplicationPolicy
   def permitted_attributes
     if create?
-      [:kind, settings: [:content, :call_to_action, :button_text, :count_text, :email_text]]
+      [:kind, settings: [
+        :content,
+        :call_to_action,
+        :button_text,
+        :count_text,
+        :email_text,
+
+        :title_text,
+        :main_color,
+        :donation_value1,
+        :donation_value2,
+        :donation_value3,
+        :donation_value4,
+        :donation_value5,
+        :payment_methods,
+        :customer_data
+        ]
+      ]
     else
       []
     end
@@ -13,3 +30,4 @@ class WidgetPolicy < ApplicationPolicy
     user.present? && record.mobilization.user == user
   end
 end
+#116588043
