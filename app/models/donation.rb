57ebb0a98dcb5ada34e4c64287adb71fc6514c24
Leticia Figueira <jlcfigueira@gmail.com>
2016-04-24 18:02:19 -0300

    [#116588007] Adds split rules to capture a transaction

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -5,10 +5,45 @@ class Donation < ActiveRecord::Base
   has_one :mobilization, through: :widget
   has_one :organization, through: :mobilization
 
+  after_create :capture_transaction
   after_create :send_mail
 
   def find_transaction
     @transaction = PagarMe::Transaction.find_by_id("468589")
+  end
+
+  def capture_transaction
+    self.transaction do
+      @transaction = find_transaction
+
+      begin
+        @transaction.capture({
+          amount: self.amount,
+          split_rules: split_rules,
+          customer: self.customer
+        })
+      rescue PagarMeError => e
+        logger.error("\n==> ERRO NA DOAÇÃO: #{e.inspect}\n")
+      end
+    end
+  end
+
+  def split_rules
+    organization_sr = PagarMe::SplitRule.new(organization_rule).create
+    city_sr = PagarMe::SplitRule.new(city_rule).create
+
+    [organization_sr, city_sr]
+  end
+
+  def organization_rule
+    recipient = Organization.find_by_name("Nossas Cidades").pagarme_recipient_id
+    { charge_processing_fee: true, liable: false, percentage: 15, recipient_id: recipient
+    }
+  end
+
+  def city_rule
+    recipient = self.organization.pagarme_recipient_id
+    { charge_processing_fee: false, liable: true, percentage: 85, recipient_id: recipient }
   end
 
   def email
