75a74a5392f26b262f14f224550435c1586fda5f
Lucas Pirola <lucaspirola@gmail.com>
2016-05-13 11:11:56 -0300

    [#110173274] Change mailer to use widget setting configs

--- app/mailers/form_entry_mailer.rb
+++ app/mailers/form_entry_mailer.rb
@@ -2,16 +2,24 @@ class FormEntryMailer < ApplicationMailer
   def thank_you_email(form_entry)
     @widget = form_entry.widget
     @mobilization = @widget.mobilization
+    ws = @widget.settings
 
-    from_address = if @mobilization.user.first_name
+    from_address = if ws['sender_name'] and ws['sender_email']
+                     "#{ws['sender_name']} <#{ws['sender_email']}>"
+                   elsif @mobilization.user.first_name
                      "#{@mobilization.user.first_name} <#{@mobilization.user.email}>"
                    else
                      @mobilization.user.email
                    end
+    subject = if ws['email_subject']
+                ws['email_subject']
+              else
+                @mobilization.name
+              end
 
     mail(
       to: form_entry.email,
-      subject: @mobilization.name,
+      subject: subject,
       from: from_address
     )
   end
