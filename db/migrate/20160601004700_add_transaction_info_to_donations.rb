7cac66add5e6f6ef7846828879042434cfef0ff1
Leticia Figueira <jlcfigueira@gmail.com>
2016-06-16 12:25:31 -0300

    [#118301453] Removes duplicated migration

--- db/migrate/20160601004700_add_transaction_info_to_donations.rb
+++
@@ -1,6 +0,0 @@
-class AddTransactionInfoToDonations < ActiveRecord::Migration
-  def change
-    add_column :donations, :transaction_id, :string
-    add_column :donations, :transaction_status, :string
-  end
-end
