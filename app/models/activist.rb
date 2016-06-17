834016becb9316b16c4636902b5fc38f68496558
Leticia Figueira <jlcfigueira@gmail.com>
2016-06-16 21:07:08 -0300

    [#118301453] Ensures activists info are saved

--- app/models/activist.rb
+++ app/models/activist.rb
@@ -2,4 +2,10 @@ class Activist < ActiveRecord::Base
   has_many :credit_cards
   has_many :donations
   has_many :addresses
+
+  before_create :normalize_phone
+
+  def normalize_phone
+    self.phone.gsub!(/\D/, '')
+  end
 end
---
+++ spec/support/stub_donation_services.rb
@@ -0,0 +1,7 @@
+def stub_donation_service
+  expect(DonationService).to receive(:run).with(anything)
+end
+
+def stub_recurrent_service
+  expect(SubscriptionService).to receive(:run).with(anything)
+end
