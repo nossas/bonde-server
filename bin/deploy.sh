dd9ed6bac196ea42ecbdd02fccc50a72a45e7a72
Lucas Pirola <lucaspirola@gmail.com>
2016-07-06 21:58:46 -0300

    fix bash if syntax

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,7 +4,7 @@ rm -rf ~/.dokku
 git clone https://github.com/dokku/dokku.git ~/.dokku
 
 DOKKU_HOST="reboo-staging.org"
-if [ "$CIRCLE_BRANCH" == "master" ]; then
+if [[ "$CIRCLE_BRANCH" == "master" ]]; then
   DOKKU_HOST="reboo.org"
 fi
 REPO_URI="dokku@$DOKKU_HOST:api"
