dc8595e717406c90cbfe870a10e4408714f13735
Lucas Pirola <lucaspirola@gmail.com>
2016-07-07 19:08:36 -0300

    Add new format to rails logger

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.logger = ::Logger.new(STDOUT)
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
