b16a45862dfa880049ffb0527e86ac1d48899fc8
Lucas Pirola <lucaspirola@gmail.com>
2016-07-07 13:19:16 -0300

    Disable error output on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+	config.action_dispatch.show_exceptions = false
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
