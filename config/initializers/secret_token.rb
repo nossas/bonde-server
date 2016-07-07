4d42fc2dfae25b778b4450933a31ae1e0dc4ded9
Lucas Pirola <lucaspirola@gmail.com>
2016-07-06 22:46:13 -0300

    Revert "Set devise secret key in config"
    This reverts commit 6e9f3329b596c75e3ecf4cfa813c37f3bc9dbf4b.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4
-# requires secret_key_base or secret_token to be defined, otherwise an
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-HubApi::Application.config.secret_key = '17c29c11480b3c16bd1be9537e13e4dae2b25dc15de7ceb23d46791dbf58545bf82c1ce2a4819fc35b626656c043ad80a1d0f89cca858d4be210d16ef29cdd6d'
+HubApi::Application.config.secret_token = '17c29c11480b3c16bd1be9537e13e4dae2b25dc15de7ceb23d46791dbf58545bf82c1ce2a4819fc35b626656c043ad80a1d0f89cca858d4be210d16ef29cdd6d'
