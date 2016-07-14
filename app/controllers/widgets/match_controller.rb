60495c6e29ff9e4425ad7e86a99b011f2c3e52ac
Antônio Roberto Silva <forevertonny@gmail.com>
2016-07-13 21:01:20 -0300

    added basic crud tests for match controller

--- app/controllers/widgets/match_controller.rb
+++ app/controllers/widgets/match_controller.rb
@@ -1,37 +1,36 @@
 class Widgets::MatchController < ApplicationController
   respond_to :json
   before_action :load_widget
+  before_action :authorize_action
   after_action :verify_authorized, except: %i[index]
   after_action :verify_policy_scoped, only: %i[index]
 
   def show
     @match = Match.find(params[:id])
-    authorize @match
     render json: @match.as_json.merge(:widget_title => @widget.settings['title_text'])
   end
 
   def create
-    authorize @widget, :update?
     @match = @widget.matches.new(match_params)
     @match.save!
     render json: @match
   end
 
   def update
-    authorize @widget, :update?
     @match = @widget.matches.find(params[:id])
-    @match.update_attribtues(match_params)
+    @match.update_attributes(match_params)
     render json: @match
   end
 
   def destroy
-    authorize @widget, :update?
+    @match = @widget.matches.find(params[:id])
+    @match.destroy
     ###
     # MAYBE WORKING :)
     ###
-    body = JSON.parse request.body.read
-    column_hash = { body['column_name'] => body['value'] }
-    @widget.matches.where(column_hash).destroy_all
+    #body = JSON.parse request.body.read
+    #column_hash = { body['column_name'] => body['value'] }
+    #@widget.matches.where(column_hash).destroy_all
     # @match.destroy!
     render json: { ok: true }
   end
@@ -48,5 +47,9 @@ class Widgets::MatchController < ApplicationController
 
   def load_widget
     @widget ||= Widget.find(params[:widget_id])
+  end
+
+  def authorize_action
+    authorize @widget, :update?
   end
 end
---
+++ spec/controllers/widgets/match_controller_spec.rb
@@ -0,0 +1,50 @@
+
+require 'rails_helper'
+
+RSpec.describe Widgets::MatchController, type: :controller do
+  let(:user) { User.make! }
+  let(:mobilization) { Mobilization.make! user: user }
+  let(:block) { Block.make! mobilization: mobilization }
+  let(:widget) { Widget.make! block: block }
+  let(:match) { Match.make! widget: widget }
+  let(:current_user) { user }
+
+  before do
+    stub_current_user(current_user)
+    match
+  end
+
+  shared_examples "user access" do
+    context "when user is mobilization owner" do
+      it { is_expected.to respond_with 200 }
+    end
+
+    context "when user is not mobilization owner" do
+      let(:current_user) { User.make! }
+      it { is_expected.to respond_with 401 }
+    end
+  end
+
+  describe "GET #show" do
+    before  { get :show, widget_id: match.widget_id, id: match.id }
+
+    it_behaves_like "user access"
+  end
+
+  describe "POST #create" do
+    before  { post :create, widget_id: match.widget_id, match: {first_choice: 'lorem', second_choice: 'ipsum', goal_image: 'goal_image2'} }
+
+    it_behaves_like "user access"
+  end
+
+  describe "PUT #update" do
+    before  { put :update, widget_id: match.widget_id, id: match.id, match: { first_choice: 'foo'} }
+
+    it_behaves_like "user access"
+  end
+
+  describe "DELETE #destroy" do
+    before  { delete :destroy, widget_id: match.widget_id, id: match.id }
+    it_behaves_like "user access"
+  end
+end
