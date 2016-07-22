70369976b4c9cabab4fb447890d4bd5c8e5bf000
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-07-22 12:23:26 -0300

    Fix form entries controller index action tests. #30

--- spec/controllers/mobilizations/form_entries_controller_spec.rb
+++ spec/controllers/mobilizations/form_entries_controller_spec.rb
@@ -5,29 +5,29 @@ RSpec.describe Mobilizations::FormEntriesController, type: :controller do
   let(:mobilization) { Mobilization.make! user: user }
   let(:block) { Block.make! mobilization: mobilization }
   let(:widget) { Widget.make! block: block }
+  let(:widget2) { Widget.make! block: block }
   let(:current_user) { user }
 
   before do
     widget
+    widget2
     stub_current_user(current_user)
   end
 
   describe "GET #index" do
-
-    context "when access with not mobilization user" do
-      let(:current_user) { User.make! }
-
-      it "should raise record not found" do
-        expect do
-          get :index, mobilization_id: mobilization.id
-        end.to raise_error ActiveRecord::RecordNotFound
-      end
-    end
-
-    context "when access with mobilization user" do
+    context "when access with user" do
       let!(:form_entry) { FormEntry.make! widget: widget }
-      before { get :index, mobilization_id: mobilization.id }
-      it { is_expected.to respond_with 200 }
+      let!(:form_entry2) { FormEntry.make! widget: widget2 }
+
+      it "should return form_entries by mobilization" do
+        get(:index, mobilization_id: mobilization.id)
+        expect(response.body).to eq([form_entry, form_entry2].to_json)
+      end
+
+      it "should return form_entries by widget_id" do
+        get(:index, mobilization_id: mobilization.id, widget_id: widget.id)
+        expect(response.body).to eq([form_entry].to_json)
+      end
     end
   end
 
