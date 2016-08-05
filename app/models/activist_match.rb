fe1e938a1d92f5793441739bfeccae66b6a01fb0
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-08-04 21:27:24 -0300

    Add mailchilp integration in activist match model. ourcities/hub-client#109

--- app/models/activist_match.rb
+++ app/models/activist_match.rb
@@ -1,33 +1,23 @@
 class ActivistMatch < ActiveRecord::Base
-  # include Mailchimpable
-  #
-  # validates :widget, presence: true
-  # belongs_to :activist
-  # belongs_to :match
-  # has_one :widget, through: :match
-  #
-  # def update_mailchimp
-  #   if(!Rails.env.test?)
-  #     subscribe_attributes =  {
-  #       FNAME: self.first_name,
-  #       LNAME: self.last_name,
-  #       EMAIL: self.email,
-  #       PHONE: self.phone || "",
-  #       CITY: self.city,
-  #       ORG: self.organization.name
-  #     }
-  #
-  #     if !self.city.present? || self.city.try(:downcase) == 'outra'
-  #       subscribe_attributes.delete(:CITY)
-  #     end
-  #
-  #     subscribe_to_list(self.email, subscribe_attributes)
-  #
-  #     subscribe_to_segment(self.widget.mailchimp_segment_id, self.email)
-  #
-  #     update_member(self.email, {
-  #       groupings: [{ id: 49, groups: [self.organization.name] }]
-  #     })
-  #   end
-  # end
+  include Mailchimpable
+
+  validates :widget, :activist, presence: true
+  belongs_to :activist
+  belongs_to :match
+  has_one :widget, through: :match
+
+  def update_mailchimp
+    if(!Rails.env.test?)
+      subscribe_attributes =  {
+        FNAME: self.activist.name,
+        EMAIL: self.activist.email
+      }
+
+      subscribe_to_list(self.email, subscribe_attributes)
+      subscribe_to_segment(self.widget.mailchimp_segment_id, self.email)
+      update_member(self.email, {
+        groupings: [{ id: 49, groups: [self.organization.name] }]
+      })
+    end
+  end
 end
