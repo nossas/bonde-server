53a5933185fedf8ae56de63653ea0ac2ce0b5cd5
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-08-18 16:25:21 -0300

    Add pressure count to widget serializer. #39

--- app/serializers/widget_serializer.rb
+++ app/serializers/widget_serializer.rb
@@ -1,10 +1,18 @@
 class WidgetSerializer < ActiveModel::Serializer
   attributes :id, :block_id, :kind, :settings, :sm_size, :md_size, :lg_size,
     :form_entries_count, :donations_count, :created_at, :updated_at,
-    :action_community, :action_opportunity, :match_list, :exported_at
+    :action_community, :action_opportunity, :exported_at, :match_list, :pressure_count
 
   def match_list
     object.matches
+  end
+
+  #
+  # Find a way to remove these attributes depending on widget kind.
+  # e.g. Is not necessary to have `pressure_count` if widget kind is match.
+  #
+  def pressure_count
+    object.activist_pressures.count
   end
 
   def settings
