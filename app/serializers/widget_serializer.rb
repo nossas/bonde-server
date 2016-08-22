b19d4b37504fdfc422b9c1bd11d5846204d9a4bf
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-08-21 21:13:42 -0300

    Change widget serializer `pressure_count` to `count`. #127

--- app/serializers/widget_serializer.rb
+++ app/serializers/widget_serializer.rb
@@ -1,17 +1,18 @@
 class WidgetSerializer < ActiveModel::Serializer
   attributes :id, :block_id, :kind, :settings, :sm_size, :md_size, :lg_size,
     :form_entries_count, :donations_count, :created_at, :updated_at,
-    :action_community, :action_opportunity, :exported_at, :match_list, :pressure_count
+    :action_community, :action_opportunity, :exported_at, :match_list, :count
 
   def match_list
     object.matches
   end
 
   #
-  # Find a way to remove these attributes depending on widget kind.
-  # e.g. Is not necessary to have `pressure_count` if widget kind is match.
+  # Find a way to remove the attributes match_list, form_entries_count and donations_count
+  # depending by the widget kind e.g. Is not necessary to have `form_entries_count` if widget
+  # kind is pressure.
   #
-  def pressure_count
+  def count
     object.activist_pressures.count
   end
 
