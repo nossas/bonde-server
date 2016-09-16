55617daa74bf32f312c771c15267dfd0e39767d2
Antônio Roberto Silva <forevertonny@gmail.com>
2016-09-16 09:25:05 -0300

    added scopes to filter payable_detail by widget/mob/block

--- app/models/payable_detail.rb
+++ app/models/payable_detail.rb
@@ -1,6 +1,10 @@
 class PayableDetail < ActiveRecord::Base
   belongs_to :organization
   default_scope { order('transaction_id desc') }
+  scope :by_widget, ->(id) { where(widget_id: id) }
+  scope :by_mobilization, ->(id) { where(mobilization_id: id) }
+  scope :by_block, ->(id) { where(block_id: id) }
+
   def self.table_name
     'public.payable_details'
   end
