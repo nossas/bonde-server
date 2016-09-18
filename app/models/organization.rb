7fbf9b7963018fa2c381730428182f7490d27376
Antônio Roberto Silva <forevertonny@gmail.com>
2016-09-17 23:12:24 -0300

    added agg methods to get / sum all pending payables to transfer

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,4 +4,12 @@ class Organization < ActiveRecord::Base
   has_many :payable_details
   has_many :mobilizations
   has_many :users, through: :mobilizations
+
+  def total_to_receive_from_subscriptions
+    @total_to_receive_from_subscriptions ||= subscription_payables_to_transfer.sum(:value_without_fee)
+  end
+
+  def subscription_payables_to_transfer
+    @subscription_payables_to_transfer ||= payable_details.is_paid.from_subscription.over_limit_to_transfer
+  end
 end
