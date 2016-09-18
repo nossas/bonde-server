d4ecd346c69f163303431c8fc35a27151af84990
Antônio Roberto Silva <forevertonny@gmail.com>
2016-09-17 22:51:53 -0300

    added self.primary_key to PayableDetail

--- app/models/payable_detail.rb
+++ app/models/payable_detail.rb
@@ -1,13 +1,12 @@
 class PayableDetail < ActiveRecord::Base
+  self.primary_key = 'payable_id'
+  self.table_name = 'public.payable_details'
+
   belongs_to :organization
   default_scope { order('transaction_id desc') }
   scope :by_widget, ->(id) { where(widget_id: id) }
   scope :by_mobilization, ->(id) { where(mobilization_id: id) }
   scope :by_block, ->(id) { where(block_id: id) }
-
-  def self.table_name
-    'public.payable_details'
-  end
 
   def readonly?
     true
