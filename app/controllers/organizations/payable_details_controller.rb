804c4dc6c637fb877bb04bc426552e0437361c51
Antônio Roberto Silva <forevertonny@gmail.com>
2016-09-17 23:17:42 -0300

    use has_scope on PayableDetail scopes for payable_details controller

--- app/controllers/organizations/payable_details_controller.rb
+++ app/controllers/organizations/payable_details_controller.rb
@@ -1,9 +1,10 @@
 class Organizations::PayableDetailsController < ApplicationController
   before_action :skip_policy_scope
+  has_scope :by_widget, :by_mobilization, :by_block
 
   def index
     authorize organization, :can_handle_with_payables?
-    render json: organization.payable_details.to_json
+    render json: apply_scopes(organization.payable_details).to_json
   end
 
   def organization
