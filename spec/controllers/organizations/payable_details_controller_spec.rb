bbf25ab71c716d23b2472a1f93d9b7d9a380af97
Antônio Roberto Silva <forevertonny@gmail.com>
2016-09-18 21:34:38 -0300

    added basic specs for payable_details controller

---
+++ spec/controllers/organizations/payable_details_controller_spec.rb
@@ -0,0 +1,76 @@
+require "rails_helper"
+
+RSpec.describe Organizations::PayableDetailsController, type: :controller do
+  let(:organization) { Organization.make! pagarme_recipient_id: 'xxx' }
+  let(:user) { User.make! }
+  let(:mobilization) { Mobilization.make!(organization: organization, user: user) }
+  let(:block) { Block.make! mobilization: mobilization }
+  let(:widget) { Widget.make!(kind: 'donation', block: block) }
+  let(:donation) do
+    Donation.make!(
+      widget: widget,
+      amount: 1000,
+      subscription: true,
+      subscription_id: 123,
+      transaction_status: 'paid',
+      transaction_id: 1234,
+      payables: [
+        {
+          id: 3992754,
+          fee: 114,
+          type: "credit",
+          amount: 1000,
+          object: "payable",
+          status: "waiting_funds",
+          installment: 1,
+          date_created: "2016-09-05T22:29:49.060Z",
+          payment_date: "2016-10-06T03:00:00.000Z",
+          recipient_id: organization.pagarme_recipient_id,
+          split_rule_id: nil,
+          payment_method: "credit_card",
+          transaction_id: 123,
+          anticipation_fee: 0,
+          bulk_anticipation_id: nil,
+          original_payment_date: nil
+        }],
+    )
+  end
+
+  before do
+    donation
+    stub_current_user(user)
+  end
+
+  describe 'GET #index' do
+    context 'when user is not on organization' do
+      let(:other_user) { User.make! }
+
+      before do
+        stub_current_user(other_user)
+        get :index, organization_id: organization.id
+      end
+
+      it "should be not authorized" do
+        expect(response).to be_unauthorized
+      end
+    end
+
+    context 'when user is on orgnization' do
+      before do
+        get :index, organization_id: organization.id
+      end
+
+      it "should be successful and render payable details" do
+        json_details = ActiveSupport::JSON.decode(response.body)[0]
+
+        expect(response).to be_successful
+        expect(json_details["value_without_fee"]).to eq(8.86)
+        expect(json_details["fee"]).to eq(1.14)
+        expect(json_details["payable_value"]).to eq(10.0)
+        expect(json_details["donation_value"]).to eq(10.0)
+        expect(json_details["pagarme_status"]).to eq('paid')
+        expect(json_details["payable_status"]).to eq('waiting_funds')
+      end
+    end
+  end
+end
