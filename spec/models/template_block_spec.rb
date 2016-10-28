3f3221ecb2dd62ae3304c43e92f983f213b7cc2d
Luiz Alb. Silva <luizalbsilva@gmail.com>
2016-10-27 22:10:20 -0200

    Spacing issues - code quality #49

--- spec/models/template_block_spec.rb
+++ spec/models/template_block_spec.rb
@@ -10,42 +10,42 @@ RSpec.describe TemplateBlock, type: :model do
   describe "create_from" do
   	context "create an instance from Block" do
       before do 
-		@block = Block.make!
-		3.times do 
-		  @block.widgets << Widget.make!(block:@block)
-		end
+				@block = Block.make!
+				3.times do 
+		  		@block.widgets << Widget.make!(block:@block)
+				end
 
-		@template = TemplateMobilization.make!
-		@templateBlock = TemplateBlock.create_from(@block, @template)
+				@template = TemplateMobilization.make!
+				@templateBlock = TemplateBlock.create_from(@block, @template)
   	  end
 
-	  it "should have mobilization_template passed by" do 
-	  	expect(@templateBlock.template_mobilization).to eq(@template)
-	  end
+	  	it "should have mobilization_template passed by" do 
+	  		expect(@templateBlock.template_mobilization).to eq(@template)
+	  	end
 
-	  it "should have same bg_class value" do 
-	  	expect(@templateBlock.bg_class).to eq(@block.bg_class)
-	  end
+	  	it "should have same bg_class value" do 
+	  		expect(@templateBlock.bg_class).to eq(@block.bg_class)
+	  	end
 	  
-	  it "should have same position value" do 
-	  	expect(@templateBlock.position).to eq(@block.position)
-	  end
-	  
-	  it "should have same hidden value" do 
-	  	expect(@templateBlock.hidden).to eq(@block.hidden)
-	  end
-	  
-	  it "should have same bg_image value" do 
-	  	expect(@templateBlock.bg_image).to eq(@block.bg_image)
-	  end
-	  
-	  it "should have same name value" do 
-	  	expect(@templateBlock.name).to eq(@block.name)
-	  end
+		  it "should have same position value" do 
+		  	expect(@templateBlock.position).to eq(@block.position)
+		  end
+		  
+		  it "should have same hidden value" do 
+		  	expect(@templateBlock.hidden).to eq(@block.hidden)
+		  end
+		  
+		  it "should have same bg_image value" do 
+		  	expect(@templateBlock.bg_image).to eq(@block.bg_image)
+		  end
+		  
+		  it "should have same name value" do 
+		  	expect(@templateBlock.name).to eq(@block.name)
+		  end
 
-	  it "should have same menu_hidden value" do 
-	  	expect(@templateBlock.menu_hidden).to eq(@block.menu_hidden)
-	  end
+		  it "should have same menu_hidden value" do 
+		  	expect(@templateBlock.menu_hidden).to eq(@block.menu_hidden)
+		  end
   	end
   end
 end
--- spec/models/template_widget_spec.rb
+++ spec/models/template_widget_spec.rb
@@ -11,46 +11,46 @@ RSpec.describe TemplateWidget, type: :model do
   describe "create_from" do
   	context "create an instance from Widget" do
       before do 
-		@widget = Widget.make!
-		@templateBlock = TemplateBlock.make!
-		@templateWidget = TemplateWidget.create_from(@widget, @templateBlock)
+				@widget = Widget.make!
+				@templateBlock = TemplateBlock.make!
+				@templateWidget = TemplateWidget.create_from(@widget, @templateBlock)
   	  end
 
-	  it "should have same template_block passed by" do 
-	  	expect(@templateWidget.template_block).to eq(@templateBlock)
-	  end
+		  it "should have same template_block passed by" do 
+		  	expect(@templateWidget.template_block).to eq(@templateBlock)
+		  end
 
-	  it "should have same settings value" do 
-	  	expect(@templateWidget.settings).to eq(@widget.settings)
-	  end
+		  it "should have same settings value" do 
+		  	expect(@templateWidget.settings).to eq(@widget.settings)
+		  end
 
-	  it "should have same kind value" do 
-	  	expect(@templateWidget.kind).to eq(@widget.kind)
-	  end
+		  it "should have same kind value" do 
+		  	expect(@templateWidget.kind).to eq(@widget.kind)
+		  end
 
-	  it "should have same sm_size value" do 
-	  	expect(@templateWidget.sm_size).to eq(@widget.sm_size)
-	  end
+		  it "should have same sm_size value" do 
+		  	expect(@templateWidget.sm_size).to eq(@widget.sm_size)
+		  end
 
-	  it "should have same md_size value" do 
-	  	expect(@templateWidget.md_size).to eq(@widget.md_size)
-	  end
+		  it "should have same md_size value" do 
+		  	expect(@templateWidget.md_size).to eq(@widget.md_size)
+		  end
 
-	  it "should have same lg_size value" do 
-	  	expect(@templateWidget.lg_size).to eq(@widget.lg_size)
-	  end
+		  it "should have same lg_size value" do 
+		  	expect(@templateWidget.lg_size).to eq(@widget.lg_size)
+		  end
 
-	  it "should have same mailchimp_segment_id value" do 
-	  	expect(@templateWidget.mailchimp_segment_id).to eq(@widget.mailchimp_segment_id)
-	  end
+		  it "should have same mailchimp_segment_id value" do 
+		  	expect(@templateWidget.mailchimp_segment_id).to eq(@widget.mailchimp_segment_id)
+		  end
 
-	  it "should have same action_community value" do 
-	  	expect(@templateWidget.action_community).to eq(@widget.action_community)
-	  end
+		  it "should have same action_community value" do 
+		  	expect(@templateWidget.action_community).to eq(@widget.action_community)
+		  end
 
-	  it "should have same exported_at value" do 
-	  	expect(@templateWidget.exported_at).to eq(@widget.exported_at)
-	  end
+		  it "should have same exported_at value" do 
+		  	expect(@templateWidget.exported_at).to eq(@widget.exported_at)
+		  end
     end
   end
 end
