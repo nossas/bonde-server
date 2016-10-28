3ac0b469f1ffa2251aec25b8768f3ecc3cf5d355
Luiz Alb. Silva <luizalbsilva@gmail.com>
2016-10-28 08:45:53 -0200

    Mobilization instance created from TemplateMobilization object #52

--- spec/models/mobilization_spec.rb
+++ spec/models/mobilization_spec.rb
@@ -41,4 +41,57 @@ RSpec.describe Mobilization, type: :model do
       expect(subject.twitter_share_text).to include subject.name
     end
   end
+
+  context "create mobilization from TemplateMobilition object" do
+    before do 
+      @template = TemplateMobilization.make!
+    end
+    subject {
+      Mobilization.create_from(@template)
+    }
+
+    it "should copy the name value" do
+      expect(subject.name).to eq(@template.name)
+    end
+
+    it "should copy the color_scheme value" do
+      expect(subject.color_scheme).to eq(@template.color_scheme)
+    end
+
+    it "should copy the facebook_share_title value" do
+      expect(subject.facebook_share_title).to eq(@template.facebook_share_title)
+    end
+
+    it "should copy the facebook_share_description value" do
+      expect(subject.facebook_share_description).to eq(@template.facebook_share_description)
+    end
+
+    it "should copy the header_font value" do
+      expect(subject.header_font).to eq(@template.header_font)
+    end
+
+    it "should copy the body_font value" do
+      expect(subject.body_font).to eq(@template.body_font)
+    end
+
+    it "should copy the facebook_share_image value" do
+      expect(subject.facebook_share_image).to eq(@template.facebook_share_image)
+    end
+
+    it "should copy the slug value" do
+      expect(subject.slug).to eq(@template.slug)
+    end
+
+    it "should copy the custom_domain value" do
+      expect(subject.custom_domain).to eq(@template.custom_domain)
+    end
+
+    it "should copy the twitter_share_text value" do
+      expect(subject.twitter_share_text).to eq(@template.twitter_share_text)
+    end
+
+    it "should copy the organization_id value" do
+      expect(subject.organization_id).to eq(@template.organization_id)
+    end
+  end
 end
