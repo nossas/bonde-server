615c1b54dd2ac0ab31f5238d12e0e18a7da5ba0d
Lucas Pirola <lucaspirola@gmail.com>
2016-11-11 18:54:42 -0200

    Set redis to env var

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,8 +2,5 @@
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-
 Resque.logger.formatter = Resque::VeryVerboseFormatter.new
-Resque.redis = resque_config[rails_env]
-
+Resque.redis = ENV['REDIS_URL']
