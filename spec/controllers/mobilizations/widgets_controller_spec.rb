d429f899c71ecfb623efa4d9441065df06dc4639
Luiz Alb. Silva <luizalbsilva@gmail.com>
2016-11-18 22:46:10 -0200

    [#133883243] Adding tests ensuring the possibility to save at lest 500 emails to pressures' target

--- spec/controllers/mobilizations/widgets_controller_spec.rb
+++ spec/controllers/mobilizations/widgets_controller_spec.rb
@@ -26,26 +26,81 @@ RSpec.describe Mobilizations::WidgetsController, type: :controller do
     end
   end
 
-  xdescribe "PUT #update" do
-    it "should update widget when current user is admin" do
-      stub_current_user(@admin)
-      put :update, update_widget_1_params
+  describe "PUT #update" do
+    context 'with valid payload' do
+      it "should update widget when current user is admin" do
+        stub_current_user(@admin)
+        put :update, update_widget_1_params
 
-      expect(response.body).to include("Widget new content")
-    end
+        expect(response.body).to include("Widget new content")
+      end
 
-    it "should update widget when current user is the mobilization's owner" do
-      stub_current_user(@widget1.mobilization.user)
-      put :update, update_widget_1_params
+      it "should update widget when current user is the mobilization's owner" do
+        stub_current_user(@widget1.mobilization.user)
+        put :update, update_widget_1_params
 
-      expect(response.body).to include("Widget new content")
-    end
+        expect(response.body).to include("Widget new content")
+      end
 
-    it "should return 401 if user is not an admin or mobilization's owner" do
-      stub_current_user(User.make!)
-      put :update, update_widget_1_params
+      it "should return 401 if user is not an admin or mobilization's owner" do
+        stub_current_user(User.make!)
+        put :update, update_widget_1_params
 
-      expect(response).to be_unauthorized
+        expect(response).to be_unauthorized
+      end
+
+      context "with huge emails(500) for target pressure"
+      before do
+        stub_current_user(@admin)
+        widget_smpl = Widget.make!
+        widget = Widget.find widget_smpl.id
+        @generated_emails = 'Pression tests from OurCities Gang <meuemail@lutas.sao.nossas.org>'
+        (2..500).each do  |idx|
+          @generated_emails += ";Pression tests from OurCities Gang #{idx} <meuemail#{idx}@lutas.sao.nossas.org>"
+        end
+
+        put :update, {
+          mobilization_id: widget.block.mobilization.id,
+          id: widget.id,
+          widget: {
+            id: widget.id,
+            block_id: widget.block.id,
+            kind: "pressure",
+            settings: {
+              title_text: "Stopping legal salary increases from judiciary",
+              main_color: "#757ef1",
+              button_text: "ighting to an end",
+              count_text: "pressures made",
+              show_counter: "true",
+              reply_email: "foo@bar.com",
+              pressure_subject: "Stopping legal salary increases from judiciary",
+              pressure_body: "Let'a stop with this $@#%! piece of @#%@#@#@ !!!!",
+              targets: @generated_emails
+            },
+            sm_size: 12,
+            md_size: 12,
+            lg_size: 12,
+            form_entries_count: 0,
+            donations_count: 0,
+            created_at: widget.created_at,
+            updated_at: widget.updated_at,
+            action_community: false,
+            action_opportunity: false,
+            exported_at: nil,
+            match_list: [],
+            count: 0        
+          },
+          format: 'json'
+        }
+      end
+
+      it 'should return 200 status' do
+        expect(response.status).to be 200
+      end
+
+      it 'should return the emails saved data' do
+        expect(response.body).to include('meuemail@lutas.sao.nossas.org')
+      end
     end
   end
 end
