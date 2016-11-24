2a0845b74ca5af334420ba6522d54db14b70abb7
Luiz Alb. Silva <luizalbsilva@gmail.com>
2016-11-24 15:23:00 -0200

    [#134922187] More spec implementations, little corrections

--- spec/controllers/widgets/fill_controller_spec.rb
+++ spec/controllers/widgets/fill_controller_spec.rb
@@ -25,12 +25,72 @@ RSpec.describe Widgets::FillController, type: :controller do
   end
 
   describe "POST #create" do
-    before {
-      post :create,
-      widget_id: widget,
-      fill: { activist: { name: 'Foo Bar', email: 'foo@bar.org', city: 'Pindamonhangaba/SP' } }
-    }
+    context 'widget kind ' do
+      before {
+        post :create, widget_id: widget, fill: { activist: { name: 'Foo Bar', email: 'foo@bar.org' } }
+      }
 
-    it_behaves_like "public access"
+      it_behaves_like "public access"
+
+      it 'should return a 200 status' do
+        expect(response.status).to be 200
+      end
+
+      it 'should return an empty object' do
+        expect(response.body).to include('{}')
+      end
+    end
+
+    context 'widget kind ' do
+      before {
+        post :create, widget_id: widget, fill: { activist: { name: 'Foo Bar', email: 'foo@bar.org', city: 'Pindamonhangaba/SP' } }
+      }
+
+      it_behaves_like "public access"
+
+      it 'should return a 200 status' do
+        expect(response.status).to be 200
+      end
+
+      it 'should return an empty object' do
+        expect(response.body).to include('{}')
+      end
+    end
+
+    context 'widget kind ' do
+      before {
+        widget.kind = 'pressure'
+        widget.save!
+        post :create, widget_id: widget, fill: { 
+          activist: { 
+            firstname: 'test201611241458', 
+            lastname: 'test201611241458', 
+            email: 'test201611241459@email.com', 
+            city: 'São Paulo'
+          }, 
+          mail: {
+            cc: [ 'target1@email.com',  
+                  'target2@email.com', 
+                  'target3@email.com', 
+                  'target4@email.com', 
+                  'target5@email.com' ], 
+            subject: 'Assunto do email!',
+            body: 'Corpo do email que será enviado.'
+          }
+        }
+      }
+
+      it_behaves_like "public access"
+
+      it 'should return a 200 status' do
+        expect(response.status).to be 200
+      end
+
+      it 'should return an activist_pressure object' do
+        expect(response.body).to include("\"widget_id\":#{widget.id}")
+        expect(response.body).to include("\"count\":")
+      end
+    end
   end
 end
+
