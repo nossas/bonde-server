e8c72009c31673eb2fbb0bf1fc3aadf60ce9cbf2
Lucas Pirola <lucaspirola@gmail.com>
2016-11-28 20:21:23 -0200

    set dokku and flynn deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,15 +6,16 @@ fi
 
 git fetch --unshallow origin
 
-git remote add deploy $REPO_URI
-
-GIT_SSL_NO_VERIFY=true git push -f deploy $CIRCLE_SHA1:refs/heads/master
-
 if [[ "$CIRCLE_BRANCH" == "master" ]]; then
   rm -rf ~/.dokku
   git clone https://github.com/dokku/dokku.git ~/.dokku
+	git remote add dokku $REPO_URI
+	GIT_SSL_NO_VERIFY=true git push -f dokku $CIRCLE_SHA1:refs/heads/master
   $HOME/.dokku/contrib/dokku_client.sh run "rake db:migrate"
 else
+	git remote add flynn $REPO_URI
+	GIT_SSL_NO_VERIFY=true git push -f flynn $CIRCLE_SHA1:refs/heads/master
+
   L=/home/ubuntu/bin/flynn && curl -sSL -A "`uname -sp`" https://dl.flynn.io/cli | zcat >$L && chmod +x $L
   flynn run rake db:migrate
 fi
