ab2191fb766081ffeb8cd99f49b2f6916d78d437
Antônio Roberto <forevertonny@gmail.com>
2016-11-30 19:12:48 -0200

    Added link_activist method
    Should link or create if not exists an activist before create
    form_entrie

--- spec/models/form_entry_spec.rb
+++ spec/models/form_entry_spec.rb
@@ -1,11 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe FormEntry, type: :model do
+  let(:activist) { Activist.make! }
+  let(:build_form_entry) { FormEntry.make }
+  let(:email) { nil }
+
 	it { should belong_to :widget }
 	it { should belong_to :activist }
 
 	it { should validate_presence_of :fields }
 	it { should validate_presence_of :widget }
+
+  before do
+    allow(build_form_entry).to receive(:email).and_return(email)
+    allow(build_form_entry).to receive(:first_name).and_return('Foo bar')
+  end
+
+  describe '#link_activist' do
+    let(:email) { activist.email }
+    subject { build_form_entry.link_activist }
+
+    context 'when activist exists' do
+      it 'should link with found activist' do
+        expect(Activist).to receive(:find_by).with(email: activist.email).and_call_original
+        subject
+        expect(build_form_entry.activist).to eq(activist)
+      end
+    end
+
+    context 'when activist does not exists' do
+      let(:email) { 'loem@lorem.com' }
+
+      it 'should build a new activist' do
+        expect(build_form_entry).to receive(:create_activist).with(name: 'Foo bar', email: email).and_call_original
+        subject
+        expect(build_form_entry.activist).to_not be_nil
+        expect(build_form_entry.activist).to_not eq(activist)
+      end
+    end
+  end
+
+  describe "before create" do
+    let(:email) { 'loem@lorem.com' }
+    it 'should call link_activist' do
+      expect(build_form_entry).to receive(:link_activist)
+      build_form_entry.save
+    end
+  end
 
 	describe "Puts a message in Resque queue" do
 		before do 
