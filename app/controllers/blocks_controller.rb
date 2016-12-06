ae657e86bcbfb9bbf376484fe164ae3af41729fb
Luiz Alb. Silva <luizalbsilva@gmail.com>
2016-12-06 17:54:29 -0200

    [#135643271] Correction recorrent errors on sentry: NoMethodError: undefined method widgets'

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -5,6 +5,7 @@ class BlocksController < ApplicationController
 
   def index
     mobilization = policy_scope(Mobilization).filter(params.slice(:custom_domain, :slug))
+    mobilization = mobilization.first if mobilization.kind_of?(Array)
     blocks = mobilization.present? ? mobilization.blocks.order(:position) : []
     render json: blocks
   end
