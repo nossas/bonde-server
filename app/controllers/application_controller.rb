8e6941209caf97efb90a4556b014f083087597fa
Lucas Pirola <lucaspirola@gmail.com>
2016-12-06 02:44:51 -0200

    Add fix to devise 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name) }
+
   end
 
   def user_not_authorized
