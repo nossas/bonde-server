fdb23dd194af893de9d5611dfa1643753c9e03a4
Luiz Alb. Silva <luizalbsilva@gmail.com>
2016-12-07 12:10:37 -0200

    [#135689997] Community creation

--- app/policies/community_policy.rb
+++ app/policies/community_policy.rb
@@ -1,9 +1,13 @@
 class CommunityPolicy < ApplicationPolicy
+  def permitted_attributes
+    [:name, :city, :pagarme, :transfer_day, :transfer_enabled]
+  end
+
   def can_handle_with_payables?
     is_owned_by?(user)
   end
 
-  private
+private
 
   def is_owned_by?(user)
     user.present? && record.users.include?(user)
--- spec/controllers/communities_controller_spec.rb
+++ spec/controllers/communities_controller_spec.rb
@@ -1,11 +1,82 @@
 require 'rails_helper'
 
 RSpec.describe CommunitiesController, type: :controller do
+  before do 
+    @user = User.make!
+
+    stub_current_user(@user)
+  end
+
   describe "GET #index" do
     it "should return all organizations" do
       3.times { Community.make! }
       get :index
       expect(response.body).to include(Community.all.to_json)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'valid call' do
+      before do
+        @count = Community.count
+        post :create, {
+          format: :json, 
+          community: {
+            name: 'José Marculino Silva',
+            city: 'Pindamonhangaba, SP'
+          }
+        }
+      end
+
+      it 'should return a 200 status' do
+        expect(response.status).to be 200
+      end
+
+      it 'should have one more register on disk' do
+        expect(Community.count).to be (@count + 1)
+      end
+
+      it 'should return the data saved' do
+        expect(response.body).to include(Community.last.to_json)
+      end
+    end
+
+    context 'user not logged' do 
+      before do
+        stub_current_user(nil)
+        @count = Community.count
+        post :create, {
+          format: :json, 
+          community: {
+            name: 'José Joselito',
+            city: 'Taubaté, SP'
+          }
+        }
+      end
+
+      it 'should return a 401 status' do
+        expect(response.status).to be 401
+      end
+    end
+
+
+    context 'Fields missing' do 
+      before do
+        post :create, {
+          format: :json, 
+          community: {
+            cidate: 'Taubaté, SP'
+          }
+        }
+      end
+
+      it 'should return a 400 status' do
+        expect(response.status).to be 400
+      end
+
+      it 'should return error messaage' do
+        expect(response.body).to be
+      end
     end
   end
 end
