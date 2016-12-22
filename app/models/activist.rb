46e588c537004f5cd56addb58a78ad43a059f5ee
Antônio Roberto <forevertonny@gmail.com>
2016-12-22 09:43:28 -0200

    Add validations on activist
    We should validate email formatting and name length

--- app/models/activist.rb
+++ app/models/activist.rb
@@ -2,4 +2,8 @@ class Activist < ActiveRecord::Base
   has_many :donations
   has_many :credit_cards, dependent: :destroy
   has_many :addresses, dependent: :destroy
+
+  validates :name, :email, presence: true
+  validates :name, length: { in: 3..70 }
+  validates_format_of :email, with: Devise.email_regexp
 end
--- spec/models/activist_spec.rb
+++ spec/models/activist_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Activist, type: :model do
   it { should have_many :donations }
   it { should have_many :addresses }
   it { should have_many :credit_cards }
+
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
+
+  it { should allow_value('lorem@lorem.com').for(:email) }
+  it { should_not allow_value('[{"uid"=>"field-1478867526541-5", "kind"=>"text", "label"=>"Nome", "placeholder"=>"", "required"=>"true", "value"=>"lorem"}]').for(:email) }
+  it { should allow_value('lorem ipsum dolor sit amet').for(:name) }
+  it { should_not allow_value('[{"uid"=>"field-1478867526541-5", "kind"=>"text", "label"=>"Nome", "placeholder"=>"", "required"=>"true", "value"=>"lorem"}]').for(:name) }
 end
