0c8327d86fda650cbb87b77d26ff12410f2b2d45
Antônio Roberto <forevertonny@gmail.com>
2016-12-22 10:45:39 -0200

    Form entry should get name via nombre too

--- app/models/activist.rb
+++ app/models/activist.rb
@@ -2,6 +2,7 @@ class Activist < ActiveRecord::Base
   has_many :donations
   has_many :credit_cards, dependent: :destroy
   has_many :addresses, dependent: :destroy
+  has_many :form_entries
 
   validates :name, :email, presence: true
   validates :name, length: { in: 3..70 }
