8b57a0a96deed53e852c87288a44bfffd7730067
Antônio Roberto <forevertonny@gmail.com>
2016-12-22 19:09:57 -0200

    mapping matches and pressures on activist

--- app/models/activist.rb
+++ app/models/activist.rb
@@ -3,6 +3,8 @@ class Activist < ActiveRecord::Base
   has_many :credit_cards, dependent: :destroy
   has_many :addresses, dependent: :destroy
   has_many :form_entries
+  has_many :activist_pressures
+  has_many :activist_matches
 
   validates :name, :email, presence: true
   validates :name, length: { in: 3..70 }
