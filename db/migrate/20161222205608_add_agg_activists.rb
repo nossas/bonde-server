b2de7d4659b417a7ba191880188675b21a280a49
Luiz Alb. Silva <luizalbsilva@gmail.com>
2017-01-04 18:50:18 -0200

    Getting rid of more rubocop offenses

--- db/migrate/20161222205608_add_agg_activists.rb
+++ db/migrate/20161222205608_add_agg_activists.rb
@@ -1,47 +1,48 @@
 class AddAggActivists < ActiveRecord::Migration
-  def up
+  def up # rubocop:disable Metrics/MethodLength
     execute %Q{
 create or replace view public.agg_activists as
-    select
-        com.id as community_id,
-        a.id as activist_id,
-        a.email,
-        a.name as name,
-        (btrim(a.phone, '{}')::hstore->'ddd')::text||(btrim(a.phone, '{}')::hstore->'number')::text  as phone,
-        count(distinct fe.id) as total_form_entries,
-        count(distinct d.id) as total_donations,
-        count(distinct ap.id) as total_pressures,
-        last_donation.transaction_status as last_donation_status,
-        last_donation.amount/100 as last_donation_amount,
-        last_donation.subscription as last_donation_is_subscription
-    from communities com
-        left join mobilizations m on m.community_id = com.id
-        left join blocks b on b.mobilization_id = m.id
-        left join widgets w on w.block_id = b.id
+  select
+    com.id as community_id,
+    a.id as activist_id,
+    a.email,
+    a.name as name,
+    (btrim(a.phone, '{}')::hstore->'ddd')::text||(btrim(a.phone, '{}')::hstore->'number')::text  as phone,
+    count(distinct fe.id) as total_form_entries,
+    count(distinct d.id) as total_donations,
+    count(distinct ap.id) as total_pressures,
+    last_donation.transaction_status as last_donation_status,
+    last_donation.amount/100 as last_donation_amount,
+    last_donation.subscription as last_donation_is_subscription
+  from communities com
+    left join mobilizations m on m.community_id = com.id
+    left join blocks b on b.mobilization_id = m.id
+    left join widgets w on w.block_id = b.id
 
-        left join form_entries fe on fe.widget_id = w.id
-        left join donations d on d.widget_id = w.id
-            and d.transaction_id is not null and d.transaction_status is not null
-        left join activist_pressures ap on ap.widget_id = w.id
+    left join form_entries fe on fe.widget_id = w.id
+    left join donations d on d.widget_id = w.id
+        and d.transaction_id is not null and d.transaction_status is not null
+    left join activist_pressures ap on ap.widget_id = w.id
 
-        left join activists a on a.id = fe.activist_id
-            or a.id = d.activist_id
-            or a.id = ap.activist_id
+    left join activists a on a.id = fe.activist_id
+        or a.id = d.activist_id
+        or a.id = ap.activist_id
 
-        left join lateral (
-            select
-                *
-            from donations d2
-            where
-                d2.activist_id = a.id
-                and d2.transaction_id is not null
-                and d2.transaction_status is not null
-            order by id desc limit 1
-        ) as last_donation on true
-    where a.id is not null
-    group by com.id, a.email, a.id, last_donation.transaction_status,
-    last_donation.amount, last_donation.subscription;
+    left join lateral (
+      select
+        *
+      from donations d2
+      where
+        d2.activist_id = a.id
+        and d2.transaction_id is not null
+        and d2.transaction_status is not null
+      order by id desc limit 1
+    ) as last_donation on true
+  where a.id is not null
+  group by com.id, a.email, a.id, last_donation.transaction_status,
+  last_donation.amount, last_donation.subscription;
 }
+    # rubocop:enable Metrics/MethodLength
   end
 
   def down
