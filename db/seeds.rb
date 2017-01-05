0a945e2935a0ae742b983430797320957300012d
Luiz Alb. Silva <luizalbsilva@gmail.com>
2017-01-05 16:05:49 -0200

    [#136971709] Seeding information on database

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,24 @@
 user = User.find_or_create_by(email: 'foo@bar.com') do |u|
   u.uid = 'foo@bar.com'
   u.provider = 'email'
-  u.password = 'foobar'
+  u.password = 'foobar!!'
   u.first_name = 'Foo'
   u.last_name = 'Bar'
-  u.admin = true
-  u.confirm!
+  u.admin = false
+  u.skip_confirmation!
 end
 
-Mobilization.create(
-  name: 'Save the Whales!',
-  user: user,
-  goal: 'More whales, more happyness',
-  color_scheme: 'meurio-scheme',
-  header_font: 'ubuntu',
-  body_font: 'open-sans'
-)
+user_admin = User.find_or_create_by(email: 'admin_foo@bar.com') do |u|
+  u.uid = 'admin_foo@bar.com'
+  u.provider = 'email'
+  u.password = 'foobar!!'
+  u.first_name = 'admin Foo'
+  u.last_name = 'Bar'
+  u.admin = true
+  u.skip_confirmation!
+end
 
-Organization.create([
+communities = Community.create([
   { name: "Minha Blumenau", city: "Blumenau" },
   { name: "Minha Campinas", city: "Campinas" },
   { name: "Minha Curitiba", city: "Curitiba" },
@@ -34,5 +35,55 @@ Organization.create([
   { name: "Minha Porto Alegre", city: "Porto Alegre" },
   { name: "Meu Recife", city: "Recife" },
   { name: "Meu Rio", city: "Rio de Janeiro" },
-  { name: "Minha Sampa", city: "São Paulo" }
+  { name: "Minha Sampa", city: "São Paulo" },
+  { name: "Nossas Cidades", city:"Nossas Cidades" },
+  { name: "Nuestras Ciudades", city:"Nuestras Ciudades" },
+  { name: "Our Cities", city:"Our Cities" },
+  { name: "Minha Jampa", city:"João Pessoa" },
+  { name: "Meu Oiapoque", city:"Oiapoque" },
+  { name: "Mi Ciudad Mx", city:"Ciudad de México" },
+  { name: "Caracas Mi Convive", city:"Caracas" },
+  { name: "Redes ayuda", city:"Redes ayuda" },
+  { name: "ASJ", city:"ASJ" },
+  { name: "Jóvenes Contra la Violencia", city:"Jóvenes Contra la Violencia" },
+  { name: "Corporación para el Desarrollo Regional", city:"Corporación para el Desarrollo Regional" },
+  { name: "Fósforo", city:"Fósforo" },
+  { name: "Enjambre digital", city:"Enjambre digital" },
+  { name: "Casa de las Estrategias", city:"Casa de las Estrategias" }
 ])
+
+communities.each do |c|
+  CommunityUser.create([
+    {  
+      user_id: user.id,
+      community_id: c.id,
+      role: 1
+    }
+  ])
+end
+
+mobilizations = Mobilization.create([
+  {
+    name: 'Vamos limpar o tietê!',
+    user: user_admin,
+    goal: 'Um rio limpo para todos',
+    color_scheme: 'minhasampa-scheme',
+    header_font: 'ubuntu',
+    body_font: 'open-sans',
+    community_id: communities.select{|c|c.name=='Minha Sampa'}[0].id
+  },
+  {
+    name: 'Save the Whales!',
+    user: user_admin,
+    goal: 'More whales, more happyness',
+    color_scheme: 'meurio-scheme',
+    header_font: 'ubuntu',
+    body_font: 'open-sans',
+    community_id: communities.select{|c|c.name=='Meu Rio'}[0].id
+  }
+])
+
+mobilizations.each do |mob|
+  block = Block.create mobilization: mob, bg_class: 'bg-1', position:1, name: 'Tô nessa'
+  Widget.create block: block, kind: ['pressure', 'form'][mob.id % 2], sm_size: 1, md_size: 2, lg_size: 4
+end
