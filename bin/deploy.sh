87fa474f703d03acbe308180462898475e38364c
Lucas Pirola <lucaspirola@gmail.com>
2017-01-26 19:53:33 -0200

    Set circleci to work with new version management

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 REPO_URI="dokku@reboo-staging.org:api-ssl"
-if [[ "$CIRCLE_BRANCH" == "master" ]]; then
+if [ ! -z "$CIRCLE_TAG" ]; then
   REPO_URI="dokku@api-ssl.reboo.org:api-ssl"
 fi
 
@@ -10,4 +10,4 @@ git push -f dokku $CIRCLE_SHA1:refs/heads/master
 
 rm -rf ~/.dokku
 git clone https://github.com/dokku/dokku.git ~/.dokku
-$HOME/.dokku/contrib/dokku_client.sh run "rake db:migrate"
+$HOME/.dokku/contrib/dokku_client.sh run "./bin/rake db:migrate"
