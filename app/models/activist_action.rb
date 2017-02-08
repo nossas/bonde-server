ab44e6b781f865791f859e3563469241db80e0ed
Antônio Roberto <forevertonny@gmail.com>
2017-02-08 18:55:23 -0200

    [Fix #157] Added activist actions controller to download report

--- app/models/activist_action.rb
+++ app/models/activist_action.rb
@@ -3,4 +3,7 @@ class ActivistAction < ActiveRecord::Base
   self.table_name = 'public.activist_actions'
 
   belongs_to :community
+
+  scope :by_widget, ->(id) { where(widget_id: id) }
+  scope :by_mobilization, ->(id) { where(mobilization_id: id) }
 end
