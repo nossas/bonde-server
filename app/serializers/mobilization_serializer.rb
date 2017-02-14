e6a6162f884c4cbb7734eee5ec189b48c53e4caf
Luiz Alb. Silva <luizalbsilva@gmail.com>
2017-02-14 19:27:35 -0200

    Issue #171 - Adding the tag system to mobilizations

---
+++ app/serializers/mobilization_serializer.rb
@@ -0,0 +1,5 @@
+class MobilizationSerializer < ActiveModel::Serializer
+  attributes :id, :name, :created_at, :updated_at, :user_id, :color_scheme, :google_analytics_code, :goal,
+             :header_font, :body_font, :facebook_share_title, :facebook_share_description, :facebook_share_image, :slug,
+             :custom_domain, :twitter_share_text, :community_id, :tag_list
+end
