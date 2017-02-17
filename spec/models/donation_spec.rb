a4b4917fbcfe78caa4cb71249d94db333378d094
Antônio Roberto <forevertonny@gmail.com>
2017-02-17 13:37:57 -0200

    Add subscription relation to donation and some scopes

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Donation, type: :model do
   it { should belong_to :widget }
   it { should belong_to :activist }
+  it { should belong_to :subscription_relation }
 
   it { should have_one :mobilization }
   it { should have_one :community }
@@ -30,6 +31,19 @@ RSpec.describe Donation, type: :model do
       expect(resque_job['args'][0]).to be_eql 52
       expect(resque_job['args'][1]).to be_eql 'donation'
       expect(resque_job['args'].size).to be 2
+    end
+  end
+
+  describe 'scopes' do
+    context 'paid' do
+      before do
+        3.times { Donation.make! transaction_status: 'paid' }
+        4.times { Donation.make! transaction_status: 'pending' }
+        2.times { Donation.make! transaction_status: 'refused' }
+      end
+
+      subject { Donation.paid.count }
+      it { is_expected.to eq(3) }
     end
   end
 end
