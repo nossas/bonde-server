2203c018cec409bd15876e5acd9ddb53a8f2c159
Antônio Roberto <forevertonny@gmail.com>
2017-03-08 11:12:16 -0300

    Added NotificationMailer to handle with parse and build mail

---
+++ spec/mailers/notification_mailer_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe NotificationMailer, type: :mailer do
+  let(:activist) { create(:activist) }
+  let(:notification_template) { create(:notification_template) }
+  let(:notification) { create(:notification, activist: activist, notification_template: notification_template, template_vars: { name: 'lorem2' }) }
+
+  describe "notify" do
+    let(:mail) { NotificationMailer.notify(notification) }
+
+    it "should parse and set contents" do
+      expect(mail.subject).to eq("hello lorem2")
+      expect(mail.to).to eq([activist.email])
+      expect(mail.body.encoded).to eq('World lorem2')
+      expect(mail['X-SMTPAPI'].present?).to eq(true)
+    end
+  end
+end
