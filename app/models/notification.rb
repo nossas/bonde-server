273ec0b1fe9dcbbfc34d42c6cbb2559ff05b13c3
Antônio Roberto <forevertonny@gmail.com>
2017-03-08 10:21:05 -0300

    Added notify! on Notifications
    Search by template and create notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,13 @@ class Notification < ActiveRecord::Base
   belongs_to :notification_template
 
   validates :activist, :notification_template, presence: true
+
+  def self.notify!(activist_id, template_name, template_vars, from_community = false)
+    notification_template = NotificationTemplate.find_by_label(template_name)
+    create!(
+      activist_id: activist_id,
+      notification_template: notification_template,
+      template_vars: template_vars.to_json
+    )
+  end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Notification, type: :model do
   it { should belong_to :notification_template}
   it { should validate_presence_of :activist}
   it { should validate_presence_of :notification_template }
+
+  describe ".notify!" do
+    let(:activist) { create(:activist) }
+    let(:notification_template) { create(:notification_template) }
+
+    subject { Notification.notify!(activist.id, notification_template.label, { name: 'lorem1'}) }
+
+    context "should create an notification for activist" do
+      before do
+        allow(NotificationTemplate).to receive(:find_by_label).with(notification_template.label).and_call_original
+      end
+
+      it do
+        expect(subject.activist).to eq(activist)
+        expect(subject.notification_template).to eq(notification_template)
+        expect(subject.template_vars).to_not be_nil
+        expect(subject.persisted?).to eq(true)
+      end
+    end
+  end
 end
