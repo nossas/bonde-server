ed822a5ccbf8d9729c44027b4d7157a669319e16
Antônio Roberto <forevertonny@gmail.com>
2017-03-08 09:00:01 -0300

    Basic validations on NotificationTemplate
    subject_template, body_template and label

--- app/models/notification_template.rb
+++ app/models/notification_template.rb
@@ -1,3 +1,4 @@
 class NotificationTemplate < ActiveRecord::Base
   belongs_to :community
+  validates :subject_template, :body_template, :label, presence: true
 end
--- spec/models/notification_template_spec.rb
+++ spec/models/notification_template_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe NotificationTemplate, type: :model do
   it { should belong_to :community}
+  it { should validate_presence_of :label }
+  it { should validate_presence_of :subject_template }
+  it { should validate_presence_of :body_template }
+
 end
