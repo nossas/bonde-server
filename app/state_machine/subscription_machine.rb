ad2c00fa86761a99ac20d9b8e8e6a840338f7b11
Antônio Roberto <forevertonny@gmail.com>
2017-03-15 18:23:45 -0300

    Notify activist on subscription event changes (paid/unpaid/canceled)

--- app/state_machine/subscription_machine.rb
+++ app/state_machine/subscription_machine.rb
@@ -5,4 +5,16 @@ class SubscriptionMachine
   state :paid
   state :unpaid
   state :canceled
+
+  after_transition(to: :paid) do |subscription|
+    subscription.notify_activist(:paid_subscription)
+  end
+
+  after_transition(to: :unpaid) do |subscription|
+    subscription.notify_activist(:unpaid_subscription)
+  end
+
+  after_transition(to: :canceled) do |subscription|
+    subscription.notify_activist(:canceled_subscription)
+  end
 end
