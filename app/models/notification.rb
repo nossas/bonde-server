91cab75b4c041844ffed06dadc8965e413e89c26
Antônio Roberto <forevertonny@gmail.com>
2017-03-15 18:17:29 -0300

    Added auto_deliver optional default true
    Auto deliver uses the sidekick to perform the notification mailer
    deliver

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,13 +4,18 @@ class Notification < ActiveRecord::Base
 
   validates :activist, :notification_template, presence: true
 
-  def self.notify!(activist_id, template_name, template_vars, from_community = false)
-    notification_template = NotificationTemplate.find_by_label(template_name)
+  def self.notify!(activist_id, template_name, template_vars, from_community = false, auto_deliver = true)
+    notification_template = NotificationTemplate.find_by_label(template_name.to_s)
     create!(
       activist_id: activist_id,
       notification_template: notification_template,
       template_vars: template_vars.to_json
     )
+    deliver! if auto_deliver
+  end
+
+  def deliver
+    NotificationWorker.perform_async(self.id)
   end
 
   def deliver_without_queue
