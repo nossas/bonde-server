cc4b594a04966e7c4344afd5355f6164a2210b96
Antônio Roberto <forevertonny@gmail.com>
2017-03-29 15:55:32 -0300

    added fb_link and twitter_link to communities (used in notification templates)

--- app/policies/community_policy.rb
+++ app/policies/community_policy.rb
@@ -1,6 +1,6 @@
 class CommunityPolicy < ApplicationPolicy
   def permitted_attributes
-    [:name, :city, :pagarme, :transfer_day, :transfer_enabled, :image, :description, :mailchimp_api_key, :mailchimp_list_id, :mailchimp_group_id]
+    [:name, :city, :pagarme, :transfer_day, :transfer_enabled, :image, :description, :mailchimp_api_key, :mailchimp_list_id, :mailchimp_group_id, :fb_link, :twitter_link]
   end
 
   def can_handle_with_payables?
