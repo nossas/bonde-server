47ff5176c8f278f3df950c515b8b9a5c3b5705bd
Luiz Alb. Silva <luizalbsilva@gmail.com>
2017-04-17 17:12:39 -0300

    Issue #224 - ns_ok forcing false on responses, if it is null

--- app/serializers/dns_hosted_zone_serializer.rb
+++ app/serializers/dns_hosted_zone_serializer.rb
@@ -1,3 +1,7 @@
 class DnsHostedZoneSerializer < ActiveModel::Serializer
   attributes :id, :community_id, :domain_name, :comment, :hosted_zone_id, :delegation_set_servers, :ns_ok
+
+  def ns_ok
+    object.ns_ok?
+  end
 end
