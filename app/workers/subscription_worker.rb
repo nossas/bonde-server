6c9e180205635e7d05ab41cc5e2e3bffb57960c2
Antônio Roberto <forevertonny@gmail.com>
2017-04-19 15:01:56 -0300

    Next schedule should be set on state machine transition to paid

--- app/workers/subscription_worker.rb
+++ app/workers/subscription_worker.rb
@@ -6,13 +6,9 @@ class SubscriptionWorker
     subscription = Subscription.find subscription_id
 
     return if !last_unpaid_id.nil? && subscription.last_transition.try(:id) != last_unpaid_id
+    return if subscription.canceled?
 
     subscription.charge_next_payment
     subscription.reload
-
-    SubscriptionWorker.perform_at(
-      subscription.next_transaction_charge_date,
-      subscription.id
-    ) unless subscription.has_pending_payments?
   end
 end
