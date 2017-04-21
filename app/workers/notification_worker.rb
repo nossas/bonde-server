a8e4d54e2512e21ed265743d87b855318bb8026b
Antônio Roberto <forevertonny@gmail.com>
2017-04-20 22:12:56 -0300

    use default queue and log on worker

--- app/workers/notification_worker.rb
+++ app/workers/notification_worker.rb
@@ -1,9 +1,9 @@
 class NotificationWorker
   include Sidekiq::Worker
-  sidekiq_options queue: :mailers
 
   def perform(notification_id)
     notification = Notification.find notification_id
+    Rails.logger.info "NotificationWorker runinng on notification #{notification.id}"
     notification.deliver_without_queue
   end
 end
