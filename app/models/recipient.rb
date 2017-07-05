4f36ac6f7c469e4acb6e0efa7965ed773ff03cc4
Antônio Roberto <forevertonny@gmail.com>
2017-07-05 13:21:12 -0300

    Added wrapper to get recipient from gateway

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -4,6 +4,7 @@ class Recipient < ActiveRecord::Base
 	validates :community, presence: true
 
 	belongs_to :community
+  has_many :balance_operations
 
   def update_from_pagarme
     raise PagarMe::PagarMeError.new "pagarme_recipient_id is empty" unless self.pagarme_recipient_id 
@@ -13,5 +14,9 @@ class Recipient < ActiveRecord::Base
     self.transfer_enabled = recipient_info.transfer_enabled
     self.recipient = recipient_info.as_json
     self.save!
+  end
+
+  def gateway_recipient
+    @gateway_recipient ||= PagarMe::Recipient.find pagarme_recipient_id
   end
 end
