d94aef8ddd9af18c5f2c45b4e5873d26836e100a
Antônio Roberto <forevertonny@gmail.com>
2017-07-07 13:16:20 -0300

    Added activist facebook bot interactions table

---
+++ app/models/activist_facebook_bot_interaction.rb
@@ -0,0 +1,6 @@
+class ActivistFacebookBotInteraction < ActiveRecord::Base
+  belongs_to :ativist
+  belongs_to :facebook_bot_configuration
+
+  validates :facebook_bot_configuation, :fb_context_recipient_id, :fb_context_sender_id, :interaction, presence: true
+end
