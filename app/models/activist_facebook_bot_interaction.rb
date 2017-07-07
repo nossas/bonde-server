b1c63d3b25b989d1f9869167223a5fa5fa440a16
Antônio Roberto <forevertonny@gmail.com>
2017-07-07 13:31:29 -0300

    fix typo on migrations

--- app/models/activist_facebook_bot_interaction.rb
+++ app/models/activist_facebook_bot_interaction.rb
@@ -1,5 +1,5 @@
 class ActivistFacebookBotInteraction < ActiveRecord::Base
-  belongs_to :ativist
+  belongs_to :activist
   belongs_to :facebook_bot_configuration
 
   validates :facebook_bot_configuation, :fb_context_recipient_id, :fb_context_sender_id, :interaction, presence: true
