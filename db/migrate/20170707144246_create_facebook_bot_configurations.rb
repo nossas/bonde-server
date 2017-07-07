1cc45498945ff44d8e878e263d09a9a5e1a8f21d
Antônio Roberto <forevertonny@gmail.com>
2017-07-07 14:45:55 -0300

    Set default column on bot configurations jsonb data

--- db/migrate/20170707144246_create_facebook_bot_configurations.rb
+++ db/migrate/20170707144246_create_facebook_bot_configurations.rb
@@ -5,7 +5,7 @@ class CreateFacebookBotConfigurations < ActiveRecord::Migration
       t.text :messenger_app_secret, null: false
       t.text :messenger_validation_token, null: false
       t.text :messenger_page_access_token, null: false
-      t.jsonb :data
+      t.jsonb :data, null: false, default: '{}'
 
       t.timestamps null: false
     end
