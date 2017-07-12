98e7ef47c6c76fd724b53adbbe5bc3023c1f0326
Antônio Roberto <forevertonny@gmail.com>
2017-07-12 12:48:45 -0300

    fix typo

--- app/models/activist_facebook_bot_interaction.rb
+++ app/models/activist_facebook_bot_interaction.rb
@@ -2,5 +2,5 @@ class ActivistFacebookBotInteraction < ActiveRecord::Base
   belongs_to :activist
   belongs_to :facebook_bot_configuration
 
-  validates :facebook_bot_configuation, :fb_context_recipient_id, :fb_context_sender_id, :interaction, presence: true
+  validates :facebook_bot_configuration, :fb_context_recipient_id, :fb_context_sender_id, :interaction, presence: true
 end
