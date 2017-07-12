ccd4c4f4b53a0b6ce441c8cb7744dd4611a6d078
Igor Santos <igr.exe@gmail.com>
2017-07-12 02:43:52 -0300

    feat(blocks): add attributes to duplicate widgets and block on create

--- app/policies/block_policy.rb
+++ app/policies/block_policy.rb
@@ -2,7 +2,60 @@ class BlockPolicy < ApplicationPolicy
   def permitted_attributes
     if create?
       [:position, :bg_class, :bg_image, :hidden, :name, :menu_hidden,
-        widgets_attributes: [:kind, :sm_size, :md_size, :lg_size]]
+        widgets_attributes: [:kind, :sm_size, :md_size, :lg_size, settings: [
+        # Can re-use settings
+        # Autofire config
+        :email_text,
+        :email_subject,
+        :sender_name,
+        :sender_email,
+
+        # Generic widget config
+        :title_text,
+        :main_color,
+        :button_text,
+        :count_text,
+        :show_counter,
+
+        # Settings specific widget
+        :whatsapp_text,
+
+        # Content Widget
+        :content,
+
+        # Pressure Widget
+        :targets,
+        :pressure_subject,
+        :pressure_body,
+        :reply_email,
+        :show_city,
+
+        # Donation widget
+        :default_donation_value,
+        :donation_value1,
+        :donation_value2,
+        :donation_value3,
+        :donation_value4,
+        :donation_value5,
+        :recurring_period,
+        :payment_type,
+        :payment_methods,
+        :customer_data,
+
+        # Match Widget
+        :choicesA,
+        :choices1,
+        :labelChoices1,
+        :labelChoicesA,
+
+        :call_to_action,
+        :action_community,
+
+        #finish messages
+        :finish_message,
+        :finish_message_type,
+        :finish_message_background
+        ]]]
     else
       []
     end
