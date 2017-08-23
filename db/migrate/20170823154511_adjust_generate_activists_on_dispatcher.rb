412185ac00e25aae5c895bf9d142fb363bd7f33b
Antônio Roberto <forevertonny@gmail.com>
2017-08-23 12:57:10 -0300

    check if mobilization is present before try check for community

---
+++ db/migrate/20170823154511_adjust_generate_activists_on_dispatcher.rb
@@ -0,0 +1,43 @@
+class AdjustGenerateActivistsOnDispatcher < ActiveRecord::Migration
+  def change
+    execute %Q{
+CREATE OR REPLACE FUNCTION public.generate_activists_from_generic_resource_with_widget()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+        declare
+            v_mobilization public.mobilizations;
+        begin
+            IF TG_TABLE_NAME in ('subscriptions', 'form_entries', 'donations', 'activist_pressures')
+                AND NEW.activist_id is not null AND NEW.widget_id is not null THEN
+
+                select distinct(m.*) from mobilizations m
+                    join blocks b on b.mobilization_id = m.id
+                    join widgets w on w.block_id = b.id
+                    where w.id = NEW.widget_id
+                    into v_mobilization;
+
+                IF v_mobilization.id IS NOT NULL THEN
+                    if not exists(select true
+                        from community_activists
+                        where community_id = v_mobilization.community_id and activist_id = NEW.activist_id) then
+                        insert into community_activists (community_id, activist_id, created_at, updated_at)
+                            values (v_mobilization.community_id, NEW.activist_id, now(), now());
+                    end if;
+
+                    if not exists(select true
+                        from mobilization_activists
+                        where mobilization_id = v_mobilization.id and activist_id = NEW.activist_id) then
+                        insert into mobilization_activists (mobilization_id, activist_id, created_at, updated_at)
+                            values (v_mobilization.id, NEW.activist_id, now(), now());
+                    end if;
+                END IF;
+
+            END IF;
+            return NEW;
+        end;
+    $function$
+;
+}
+  end
+end
