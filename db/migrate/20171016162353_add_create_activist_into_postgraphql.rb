313ffd3d887661e6f2341e14ce8f63dc3c0b3f39
Antônio Roberto <forevertonny@gmail.com>
2017-10-18 13:00:23 -0200

    missing sql version on buffer

--- db/migrate/20171016162353_add_create_activist_into_postgraphql.rb
+++ db/migrate/20171016162353_add_create_activist_into_postgraphql.rb
@@ -43,7 +43,10 @@ update activist_tags resource
     set activist_id = af.min_id
     from first_email_ids_activists af
         where resource.activist_id <> af.min_id
-            and resource.activist_id = ANY(af.ids);
+            and resource.activist_id = ANY(af.ids)
+            and not exists(select true from activist_tags at2
+              where at2.activist_id = resource.activist_id
+              and at2.community_id = resource.community_id);
 
 update notifications resource
     set activist_id = af.min_id
@@ -83,10 +86,9 @@ delete from activist_pressures ma where ma.id in (
     where not unaccent(lower(btrim(a.email))) ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$'
 );
 
-delete from activist_tags ma where ma.id in (
-    select m2.id from activist_tags m2
-    join activists a on a.id = m2.activist_id
-    where not unaccent(lower(btrim(a.email))) ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$'
+delete from activist_tags where activist_id in (
+    select id from activists
+    where not email ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$'
 );
 
 delete from activists where not email ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$';
@@ -94,11 +96,10 @@ delete from activists where not email ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-z
 
 create unique index uniq_email_acts on activists(lower(email::email));
 
-create or replace function postgraphql.create_activist(activist json)
-    returns json
-    language plpgsql
-    volatile
-    as $$
+CREATE OR REPLACE FUNCTION postgraphql.create_activist(activist json)
+ RETURNS json
+ LANGUAGE plpgsql
+AS $function$
         declare
             _activist public.activists;
             _community_id integer;
@@ -106,30 +107,55 @@ create or replace function postgraphql.create_activist(activist json)
         begin
             _community_id := ($1->>'community_id')::integer;
 
-            if not postgraphql.current_user_has_community_participation(_community_id) then
-                raise 'operation not permitted';
-            end if;
-
             if _community_id is null then
                 raise 'missing community_id inside activist';
+            end if;            
+
+            if not postgraphql.current_user_has_community_participation(_community_id) then
+                raise 'operation not permitted';
             end if;
 
             select * from public.mobilizations
                 where community_id = _community_id
                     and id = ($1->>'mobilization_id')::integer
                 into _mobilization;
-
-            insert into public.activists (name, email, phone, document_number, document_type, city, created_at, updated_at)
-                values ($2->>'name'::text, $2->>'email', $2->>'phone'::text, $2->>'document_number'::text,
-                    $2->>'document_type'::text, $2->>'city'::text, now(), now())
-                returning * into _activist;
-
-            insert into public.community_activists (community_id, activist_id, created_at, updated_at)
-                values ($1, _activist.id, now(), now());
+            
+            select * from public.activists a
+                where a.email = lower(($1->>'email')::email)
+                limit 1 into _activist;
+                
+            if _activist.id is null then 
+                insert into public.activists (name, email, phone, document_number, document_type, city, created_at, updated_at)
+                    values ($1->>'name'::text, lower($1->>'email'), $1->>'phone'::text, $1->>'document_number'::text,
+                        $1->>'document_type'::text, $1->>'city'::text, now(), now())
+                    returning * into _activist;
+            end if;
+            
+            if not exists(select true 
+                from public.community_activists 
+                where community_id = _community_id 
+                    and activist_id = _activist.id
+            ) then
+                insert into public.community_activists (community_id, activist_id, created_at, updated_at)
+                    values (_community_id, _activist.id, now(), now());
+            end if;
+            
+            if _mobilization.id is not null and not exists(select true 
+                from public.mobilization_activists 
+                where mobilization_id = _mobilization.id
+                    and activist_id = _activist.id
+            ) then
+                insert into public.mobilization_activists (mobilization_id, activist_id, created_at, updated_at)
+                    values (_mobilization.id, _activist.id, now(), now());
+            end if;            
 
             return row_to_json(_activist);
         end;
-    $$;
+    $function$;
+
+grant insert on public.activists to common_user, admin;
+grant insert on public.community_activists to common_user, admin;
+grant usage on sequence community_activists_id_seq to common_user, admin;
 }
   end
 
