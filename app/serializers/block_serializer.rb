5931ad874c2340a254e5f0092f44c6413d1ca4d7
Antônio Roberto <forevertonny@gmail.com>
2017-10-31 08:35:34 -0200

    fixes specs

--- app/serializers/block_serializer.rb
+++ app/serializers/block_serializer.rb
@@ -1,8 +1,8 @@
 class BlockSerializer < ActiveModel::Serializer
-    attributes :id, :mobilization_id, :created_at, :updated_at, :bg_class, :position, :hidden, :bg_image, :name, :menu_hidden
+    attributes :id, :mobilization_id, :created_at, :updated_at, :bg_class, :position, :hidden, :bg_image, :name, :menu_hidden, :deleted_at
 
   class CompleteBlockSerializer < ActiveModel::Serializer
-    attributes :id, :mobilization_id, :created_at, :updated_at, :bg_class, :position, :hidden, :bg_image, :name, :menu_hidden, :widgets_attributes
+    attributes :id, :mobilization_id, :created_at, :updated_at, :bg_class, :position, :hidden, :bg_image, :name, :menu_hidden, :widgets_attributes, :deleted_at
 
     def widgets_attributes
       object.widgets.order(:id) 
