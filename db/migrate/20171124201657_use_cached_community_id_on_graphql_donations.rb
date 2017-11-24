4a9189f29595c6852e613399e2cd0f89597da6f3
Antônio Roberto <forevertonny@gmail.com>
2017-11-24 18:45:14 -0200

    graphql donations should use cached_community_id

---
+++ db/migrate/20171124201657_use_cached_community_id_on_graphql_donations.rb
@@ -0,0 +1,61 @@
+class UseCachedCommunityIdOnGraphqlDonations < ActiveRecord::Migration
+  def up
+    execute %Q{
+CREATE OR REPLACE VIEW "postgraphql"."donations" AS 
+ SELECT d.id AS donation_id,
+    coalesce(c.id, d.cached_community_id) AS community_id,
+    w.id AS widget_id,
+    m.id AS mobilization_id,
+    b.id AS block_id,
+    d.activist_id,
+    d.email AS donation_email,
+    (d.amount / 100) AS donation_amount,
+    d.local_subscription_id AS subscription_id,
+    d.transaction_status,
+    COALESCE(((d.gateway_data ->> 'date_created'::text))::timestamp without time zone, d.created_at) AS payment_date,
+    pd.payable_date,
+    pd.payable_value AS payable_amount,
+    pd.payable_status,
+    s.status AS subscription_status
+   FROM ((((((donations d
+     JOIN widgets w ON ((w.id = d.widget_id)))
+     LEFT JOIN blocks b ON ((b.id = w.block_id)))
+     LEFT JOIN mobilizations m ON ((m.id = b.mobilization_id)))
+     LEFT JOIN communities c ON ((c.id = m.community_id OR c.id = d.cached_community_id)))
+     LEFT JOIN subscriptions s ON ((s.id = d.local_subscription_id)))
+     LEFT JOIN payable_details pd ON ((pd.donation_id = d.id)))
+  WHERE ((d.transaction_id IS NOT NULL) AND (c.id IN ( SELECT community_user_roles.community_id
+           FROM postgraphql.community_user_roles)));
+}
+  end
+
+  def down
+    execute %Q{
+CREATE OR REPLACE VIEW "postgraphql"."donations" AS 
+ SELECT d.id AS donation_id,
+    c.id AS community_id,
+    w.id AS widget_id,
+    m.id AS mobilization_id,
+    b.id AS block_id,
+    d.activist_id,
+    d.email AS donation_email,
+    (d.amount / 100) AS donation_amount,
+    d.local_subscription_id AS subscription_id,
+    d.transaction_status,
+    COALESCE(((d.gateway_data ->> 'date_created'::text))::timestamp without time zone, d.created_at) AS payment_date,
+    pd.payable_date,
+    pd.payable_value AS payable_amount,
+    pd.payable_status,
+    s.status AS subscription_status
+   FROM ((((((donations d
+     JOIN widgets w ON ((w.id = d.widget_id)))
+     JOIN blocks b ON ((b.id = w.block_id)))
+     JOIN mobilizations m ON ((m.id = b.mobilization_id)))
+     JOIN communities c ON ((c.id = m.community_id)))
+     LEFT JOIN subscriptions s ON ((s.id = d.local_subscription_id)))
+     LEFT JOIN payable_details pd ON ((pd.donation_id = d.id)))
+  WHERE ((d.transaction_id IS NOT NULL) AND (c.id IN ( SELECT community_user_roles.community_id
+           FROM postgraphql.community_user_roles)));
+}
+  end
+end
