cbcdd61c22b19a29ac79782a5b1973ecf753afbc
ton <forevertonny@gmail.com>
2017-11-24 17:27:55 -0200

    should use not deleted on blocks controller (#395)

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -6,7 +6,7 @@ class BlocksController < ApplicationController
   def index
     mobilization = policy_scope(Mobilization).filter(params.slice(:custom_domain, :slug))
     mobilization = mobilization.first if mobilization.kind_of?(Array)
-    blocks = mobilization.present? ? mobilization.blocks.order(:position) : []
+    blocks = mobilization.present? ? mobilization.blocks.not_deleted.order(:position) : []
     render json: blocks
   end
 end
