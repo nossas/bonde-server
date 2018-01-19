3b6f7653256c278d1123353a2ad5938e950a754f
Antônio Roberto <forevertonny@gmail.com>
2018-01-19 16:21:24 -0200

    Add payable_fee function that returns the fee on some donation

---
+++ spec/sql-specs/public/function.payable_fee_test.sql
@@ -0,0 +1,76 @@
+BEGIN;
+    select plan(4);
+
+    select function_returns('public', 'payable_fee', ARRAY['donations'], 'numeric');
+
+    create or replace function test_donations_with_split()
+    returns setof text language plpgsql as $$
+        declare
+            _donation public.donations;
+        begin
+
+            -- insert donation with payables
+            insert into donations(created_at, updated_at, payables)
+                values (now(), now(), json_build_array(
+                    json_build_object(
+                        'id', 'payable_id_1',
+                        'fee', 0,
+                        'amount', 1000
+                    ),
+                    json_build_object(
+                        'id', 'payable_id_2_tax',
+                        'fee', 450,
+                        'amount', 8700
+                    )
+                )::jsonb)
+                returning * into _donation;
+
+            return next is(public.payable_fee(_donation), (8700/100.0)::decimal);
+        end;
+    $$;
+    select * from test_donations_with_split();
+
+    create or replace function test_donations_with_one_payable()
+    returns setof text language plpgsql as $$
+        declare
+            _donation public.donations;
+        begin
+            -- insert donation with payables
+            insert into donations(amount, created_at, updated_at, payables)
+                values (1000, now(), now(), json_build_array(
+                    json_build_object(
+                        'id', 'payable_id_1',
+                        'fee', 0,
+                        'amount', 1000
+                    )
+                )::jsonb)
+                returning * into _donation;
+
+            return next is(
+                public.payable_fee(_donation),
+                ((1000*0.13)/100.0)::decimal,
+                'shoul calculate 13% of tax over donation amount'
+            );
+
+            -- insert donation with payables
+            insert into donations(amount, created_at, updated_at, payables)
+                values (1000, '01-01-2016'::timestamp, now(), json_build_array(
+                    json_build_object(
+                        'id', 'payable_id_1',
+                        'fee', 0,
+                        'amount', 1000
+                    )
+                )::jsonb)
+                returning * into _donation;
+
+            return next is(
+                public.payable_fee(_donation),
+                ((1000*0.15)/100.0)::decimal,
+                'shoul calculate 15% of tax over older(<=2016) donation amount'
+            );
+        end;
+    $$;
+    select * from test_donations_with_one_payable();
+
+    select * from finish();
+ROLLBACK;
