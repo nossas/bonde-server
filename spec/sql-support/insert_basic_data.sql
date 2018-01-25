125c2e1796e0787076c205b63e40b4737b7187b7
Antônio Roberto <forevertonny@gmail.com>
2018-01-25 19:47:12 -0200

    added slug to avoid not null constraint

--- spec/sql-support/insert_basic_data.sql
+++ spec/sql-support/insert_basic_data.sql
@@ -16,8 +16,8 @@ returns integer language sql as $$ select 99999 $$;
 insert into public.communities (id, name, created_at, updated_at)
     values (__demo_community_id(), 'demo_com', now(), now());
 
-insert into mobilizations (id, created_at, updated_at, name, community_id)
-    values (__demo_mobilization_id(), now(), now(), 'mob_demo', __demo_community_id());
+insert into mobilizations (id, created_at, updated_at, name, community_id, slug)
+    values (__demo_mobilization_id(), now(), now(), 'mob_demo', __demo_community_id(), 'mob_slug');
 
 insert into blocks (id, name, created_at, updated_at, mobilization_id)
     values (__demo_block_id(), 'demo_block', now(), now(), __demo_mobilization_id());
