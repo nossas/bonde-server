deb125f82cb1c694888050fe4dbe79641833b34f
Hudson Couto <hudson.sama@gmail.com>
2018-01-26 12:00:57 -0200

    fix migration to update blocks when position is nil to last position +1

--- db/migrate/20180122204907_change_position_blocks.rb
+++ db/migrate/20180122204907_change_position_blocks.rb
@@ -2,11 +2,13 @@ class ChangePositionBlocks < ActiveRecord::Migration
   def change
     Mobilization.find_each do |mobi|
       if mobi.blocks.where(position: nil).count >= 1
-        x = 0
-        mobi.blocks.where(deleted_at: nil).order(:id).each do |block|
-          block.position = x + 1
+        x = 1
+        last_block = mobi.blocks.where("deleted_at is null and position is not null").order(position: :desc).first
+        mobi.blocks.where("deleted_at is null and position is null").order(:id).each do |block|
+          block.position = last_block.position + x
           block.save
-          puts "position updated"
+          puts "Block: #{block.id} UPDATED"
+          x + 1
         end
       end
     end
