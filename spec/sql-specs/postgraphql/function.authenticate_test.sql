c3eb745fad78c0f804e9e29edd80131b48d176bb
Antônio Roberto <forevertonny@gmail.com>
2018-02-07 15:53:23 -0200

    basic test on authenticate

---
+++ spec/sql-specs/postgraphql/function.authenticate_test.sql
@@ -0,0 +1,46 @@
+BEGIN;
+    -- insert test user
+    insert into public.users(id, email, provider, uid, encrypted_password, admin)
+        values (1, 'foo@foo.com', 'bonde', '1', crypt('123456', gen_salt('bf', 9)), false);
+
+    select plan(8);
+
+    select has_function(
+        'postgraphql', 'authenticate', ARRAY['text', 'text']
+    );
+
+    select function_returns(
+        'postgraphql', 'authenticate', ARRAY['text', 'text'], 'postgraphql.jwt_token'
+    );
+
+    create or replace function test_authenticate()
+    returns setof text language plpgsql as $$
+    declare
+        _user public.users;
+        _token postgraphql.jwt_token;
+    begin
+        select * from public.users where id = 1
+        into _user;
+
+        -- try auth with invalid password
+        _token := postgraphql.authenticate('foo@foo.com', '123');
+        return next is(_token, null, 'should not get a token');
+
+        -- try auth with valid pass
+        _token := postgraphql.authenticate('foo@foo.com', '123456');
+        return next is(_token is null, false, 'should get a token');
+        return next is(_token.role, 'common_user', 'should role be setted has common_user');
+        return next is(_token.user_id, _user.id, 'should user_id be setted with authenticated user_id');
+
+        -- update to admin true
+        update users set admin = true where id = 1;
+        _token := postgraphql.authenticate('foo@foo.com', '123456');
+        return next is(_token.role, 'admin', 'should role be setted has admin');
+        return next is(_token.user_id, _user.id, 'should user_id be setted with authenticated user_id');
+
+    end;
+    $$;
+    select * from test_authenticate();
+
+ROLLBACK;
+
