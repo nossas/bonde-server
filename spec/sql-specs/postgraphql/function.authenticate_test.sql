a1cbd8c03e9e0de2baf439760e41e91464dec06c
Antônio Roberto <forevertonny@gmail.com>
2018-02-07 17:30:24 -0200

    adjust authenticate test to use local roles

--- spec/sql-specs/postgraphql/function.authenticate_test.sql
+++ spec/sql-specs/postgraphql/function.authenticate_test.sql
@@ -13,6 +13,7 @@ BEGIN;
         'postgraphql', 'authenticate', ARRAY['text', 'text'], 'postgraphql.jwt_token'
     );
 
+
     create or replace function test_authenticate()
     returns setof text language plpgsql as $$
     declare
@@ -21,6 +22,8 @@ BEGIN;
     begin
         select * from public.users where id = 1
         into _user;
+
+        set local role anonymous;
 
         -- try auth with invalid password
         _token := postgraphql.authenticate('foo@foo.com', '123');
@@ -31,16 +34,29 @@ BEGIN;
         return next is(_token is null, false, 'should get a token');
         return next is(_token.role, 'common_user', 'should role be setted has common_user');
         return next is(_token.user_id, _user.id, 'should user_id be setted with authenticated user_id');
-
-        -- update to admin true
-        update users set admin = true where id = 1;
-        _token := postgraphql.authenticate('foo@foo.com', '123456');
-        return next is(_token.role, 'admin', 'should role be setted has admin');
-        return next is(_token.user_id, _user.id, 'should user_id be setted with authenticated user_id');
-
+        set local role postgres;
     end;
     $$;
     select * from test_authenticate();
 
+    create or replace function test_authenticate_with_admin()
+    returns setof text language plpgsql as $$
+    declare
+        _user public.users;
+        _token postgraphql.jwt_token;
+    begin
+        select * from public.users where id = 1
+        into _user;
+        -- update to admin true
+        update users set admin = true where id = 1;
+
+        set local role anonymous;
+
+        _token := postgraphql.authenticate('foo@foo.com', '123456');
+        return next is(_token.role, 'admin', 'should role be setted has admin');
+        return next is(_token.user_id, _user.id, 'should user_id be setted with authenticated user_id');
+    end;
+    $$;
+    select * from test_authenticate_with_admin();
 ROLLBACK;
 
