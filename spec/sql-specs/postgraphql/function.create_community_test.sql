5b4d9ce5ecad7d0abfd18b63a83cf7080b54d693
Hudson Couto <hudson.sama@gmail.com>
2018-03-04 17:02:08 -0300

    Revert create community

--- spec/sql-specs/postgraphql/function.create_community_test.sql
+++ spec/sql-specs/postgraphql/function.create_community_test.sql
@@ -2,7 +2,7 @@ begin;
   insert into public.users(id, email, provider, uid, encrypted_password, admin, locale) values
     (1, 'foo@foo.com', 'bonde', '1', crypt('123456', gen_salt('bf', 9)), false, 'pt-BR');
 
-  select plan(8);
+  select plan(7);
 
   select has_function('postgraphql', 'create_community', ARRAY['json']);
   select function_returns('postgraphql', 'create_community', ARRAY['json'], 'json');
@@ -25,9 +25,9 @@ begin;
   create or replace function test_create_community_authenticated()
   returns setof text language plpgsql as $$
   declare
+    /* _community public.communities; */
     _community json;
   begin
-    set jwt.claims.user_id = 1;
     set local role common_user;
 
     -- test missing requied attributes
@@ -49,13 +49,6 @@ begin;
     );
     return next is(_community->>'name', 'Nossa BH', 'should community name equals Nossa BH');
     return next is(_community->>'city', 'Belo Horizonte', 'should community city equals Belo Horizonte');
-
-    return next ok(
-      (
-        select count(1) from public.community_users
-        where user_id = postgraphql.current_user_id() and community_id = (_community->>'id')::int
-      ) > 0,
-      'should create community_users after create a new community');
   end;
   $$;
   select * from test_create_community_authenticated();
