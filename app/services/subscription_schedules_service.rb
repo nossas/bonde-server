5a8990754eecf106c42470c79570d15d5e8c3544
Hudson Couto <hudson.sama@gmail.com>
2018-07-05 15:13:38 -0300

    created method can_process to verify if subscription should be process (create a new/next donation)

--- app/services/subscription_schedules_service.rb
+++ app/services/subscription_schedules_service.rb
@@ -1,17 +1,19 @@
 class SubscriptionSchedulesService
-
   def self.schedule_charges(subscription)
-    current_state = subscription.current_state
-    can_process = (
-      current_state == 'paid' || (
-        subscription.current_state == 'unpaid' &&
-        !subscription.reached_retry_limit? && subscription.reached_retry_interval?))
-
-    if can_process
+    if can_process(subscription)
       if subscription.next_transaction_charge_date <= DateTime.now
         Rails.logger.info "Creating next payment for subscription -> #{subscription.id}"
         SubscriptionWorker.perform_async(subscription.id)
       end
     end
+  end
+
+  def self.can_process(subscription)
+    current_state = subscription.current_state
+
+    (
+      current_state == 'paid' || (
+        subscription.current_state == 'unpaid' &&
+        !subscription.reached_retry_limit? && subscription.reached_retry_interval?))
   end
 end
