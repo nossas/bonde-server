60862d084665cc32e92c0ae38518ee8e0c5e74d7
Hudson Couto <hudson.sama@gmail.com>
2018-07-09 15:33:35 -0300

    changed the name of the method can_process to have a meaning as a question

--- app/services/subscription_schedules_service.rb
+++ app/services/subscription_schedules_service.rb
@@ -1,6 +1,6 @@
 class SubscriptionSchedulesService
   def self.schedule_charges(subscription)
-    if can_process(subscription)
+    if can_process?(subscription)
       if subscription.next_transaction_charge_date <= DateTime.now
         Rails.logger.info "Creating next payment for subscription -> #{subscription.id}"
         SubscriptionWorker.perform_async(subscription.id)
@@ -8,7 +8,7 @@ class SubscriptionSchedulesService
     end
   end
 
-  def self.can_process(subscription)
+  def self.can_process?(subscription)
     current_state = subscription.current_state
 
     (
