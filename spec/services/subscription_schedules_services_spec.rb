0ca6f77cd2717ea94c6a0c224b2aba5dcc979d43
Hudson Couto <hudson.sama@gmail.com>
2018-07-09 15:37:51 -0300

    added more tests where the subscription have more donations with status unpaid

--- spec/services/subscription_schedules_services_spec.rb
+++ spec/services/subscription_schedules_services_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SubscriptionSchedulesService do
+
   let(:subscription) { Subscription.make!(card_data: { id: 'card_xpto_id'}) }
 
   describe '#can_process' do
@@ -18,13 +19,13 @@ RSpec.describe SubscriptionSchedulesService do
 
     context 'when the next charges should be created' do
       it 'when the subscription it is paid and next charge is valid' do
-        expect(SubscriptionSchedulesService.can_process(subscription)).to eq(true)
+        expect(SubscriptionSchedulesService.can_process?(subscription)).to eq(true)
       end
 
       it 'when the subscription is unpaid and next reached charge is valid' do
         subscription.transition_to(:unpaid)
         donation.update_columns(transaction_status: 'refused')
-        expect(SubscriptionSchedulesService.can_process(subscription)).to eq(true)
+        expect(SubscriptionSchedulesService.can_process?(subscription)).to eq(true)
       end
     end
 
@@ -33,15 +34,43 @@ RSpec.describe SubscriptionSchedulesService do
         subscription.transition_to(:unpaid)
         donation.update_columns(transaction_status: 'refused')
         donation.update_columns(created_at: DateTime.now - 6.days)
-        expect(SubscriptionSchedulesService.can_process(subscription)).to eq(false)
+        expect(SubscriptionSchedulesService.can_process?(subscription)).to eq(false)
       end
 
       it 'when the subscription is not paid, last_donation paid and retry interval is less of community config' do
         subscription.transition_to(:unpaid)
         donation.update_columns(transaction_status: 'paid')
         donation.update_columns(created_at: DateTime.now - 6.days)
-        expect(SubscriptionSchedulesService.can_process(subscription)).to eq(false)
+        expect(SubscriptionSchedulesService.can_process?(subscription)).to eq(false)
       end
     end
+
+    context 'when the subscription has more donations unpaid' do
+			let!(:donation_1) { Donation.make!(
+				transaction_status: 'refused',
+				created_at: '05/04/2018',
+				local_subscription_id: subscription.id ) }
+			let!(:donation_2) { Donation.make!(
+				transaction_status: 'refused',
+				created_at: '05/05/2018',
+				local_subscription_id: subscription.id ) }
+			let!(:donation_3) { Donation.make!(
+				transaction_status: 'refused',
+				created_at: '05/06/2018',
+				local_subscription_id: subscription.id ) }
+
+      it 'when the subscription must have more donations with last three unpaid and can be processed' do
+				subscription.transition_to(:unpaid)
+				donation.update_columns(created_at: DateTime.now - 4.months)
+				expect(SubscriptionSchedulesService.can_process?(subscription)).to eq(true)
+			end
+
+			it 'when the subscription must have more donations and not can be processed' do
+				subscription.transition_to(:unpaid)
+				donation.update_columns(created_at: DateTime.now - 4.months)
+				donation_3.update_columns(created_at: DateTime.now - 2.days)
+				expect(SubscriptionSchedulesService.can_process?(subscription)).to eq(false)
+			end
+		end
   end
 end
