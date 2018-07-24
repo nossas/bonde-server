91786001a7fb5053d9be110c9c279cf9405c8b5c
Hudson Couto <hudson.sama@gmail.com>
2018-07-24 18:12:13 -0300

    get notification using private method

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,10 +1,17 @@
 class NotificationsController < ApplicationController
   skip_after_action :verify_authorized
   skip_after_action :verify_policy_scoped
+  before_filter :get_notification
+
   def show
     raise 'not found' unless Rails.env.staging? or Rails.env.development?
-    notification = Notification.find params[:id]
+    notification = get_notification
 
     render html: notification.mail.body.to_s.html_safe, layout: 'layouts/notifications'
+  end
+
+  private
+  def get_notification
+    Notification.find params[:id]
   end
 end
