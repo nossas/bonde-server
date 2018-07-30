73b502cde6ec487160e8a81ca10a0c7117368722
Hudson Couto <hudson.sama@gmail.com>
2018-07-30 18:27:07 -0300

    fix(template_mobilizations): changed how to create a widget in templates_mobilizations for do not receives mailchimp_segment_id

--- app/models/template_widget.rb
+++ app/models/template_widget.rb
@@ -1,6 +1,5 @@
 class TemplateWidget < ActiveRecord::Base
   validates :sm_size, :md_size, :lg_size, :kind, presence: true
-  validates :mailchimp_segment_id, uniqueness: true, allow_nil: true
   belongs_to :template_block
   has_one :mobilization, through: :template_block
   store_accessor :settings
@@ -15,7 +14,6 @@ class TemplateWidget < ActiveRecord::Base
   	template.sm_size = widget.sm_size
   	template.md_size = widget.md_size
   	template.lg_size = widget.lg_size
-  	template.mailchimp_segment_id = widget.mailchimp_segment_id
   	template.action_community = widget.action_community
   	template.exported_at = widget.exported_at
   	template
