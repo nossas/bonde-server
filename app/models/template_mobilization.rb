d66c9dd0eb84824c221382f3145235ab6f281c6c
Hudson Couto <hudson.sama@gmail.com>
2018-08-09 11:44:24 -0300

    fix(template_mobilization): removes fields not necessary when create template

--- app/models/template_mobilization.rb
+++ app/models/template_mobilization.rb
@@ -13,13 +13,10 @@ class TemplateMobilization < ActiveRecord::Base
   	template = TemplateMobilization.new
   	template.name = mobilization.name
   	template.color_scheme = mobilization.color_scheme
-  	template.facebook_share_title = mobilization.facebook_share_title
-  	template.facebook_share_description = mobilization.facebook_share_description
   	template.header_font = mobilization.header_font
   	template.body_font = mobilization.body_font
   	template.facebook_share_image = mobilization.facebook_share_image
-  	template.slug = mobilization.slug
-  	template.twitter_share_text = mobilization.twitter_share_text
+    #TODO discus about implementation of community context
   	template.community_id = mobilization.community_id
   	template
   end
