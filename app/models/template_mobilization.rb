f4cbf915c658dc499abb887612d9a0abbf94957c
Hudson Couto <hudson.sama@gmail.com>
2018-08-09 18:20:10 -0300

    fix(template_mobilization): removes presence uniques validation for slug

--- app/models/template_mobilization.rb
+++ app/models/template_mobilization.rb
@@ -2,7 +2,7 @@ class TemplateMobilization < ActiveRecord::Base
   include Shareable
   include Filterable
 
-  validates :name, :user_id, :slug, presence: true
+  validates :name, :user_id, presence: true
   belongs_to :user
   belongs_to :community
 
@@ -10,15 +10,16 @@ class TemplateMobilization < ActiveRecord::Base
   has_many :template_widgets, through: :template_blocks
 
   def self.create_from mobilization
-  	template = TemplateMobilization.new
-  	template.name = mobilization.name
-  	template.color_scheme = mobilization.color_scheme
-  	template.header_font = mobilization.header_font
-  	template.body_font = mobilization.body_font
-  	template.facebook_share_image = mobilization.facebook_share_image
-    #TODO discus about implementation of community context
-  	template.community_id = mobilization.community_id
-  	template
+    template = TemplateMobilization.new
+    template.name = mobilization.name
+    template.color_scheme = mobilization.color_scheme
+    template.header_font = mobilization.header_font
+    template.body_font = mobilization.body_font
+    template.facebook_share_image = mobilization.facebook_share_image
+
+    # TODO: discus about implementation of community context
+    template.community_id = mobilization.community_id
+    template
   end
 
 end
