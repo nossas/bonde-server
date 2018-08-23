631ea695ff590946f04830bf36d33697ab72d1f3
Hudson Couto <hudson.sama@gmail.com>
2018-08-23 18:10:04 -0300

    chore(notification/specs): add more tests for notification mailer

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -47,4 +47,36 @@ RSpec.describe NotificationMailer, type: :mailer do
       end
     end
   end
+
+  describe "#auto_fire" do
+    let(:activist) { create(:activist, email: "activist@gmail.com") }
+    let(:user) { create(:user) }
+    let(:community) { create(:community, email_template_from: 'custom@email.com') }
+    let(:template_donation) { create(:notification_template, label: 'thank_you_donation', subject_template: '{{subject}}', body_template: "{{body}}") }
+    let(:template_form_entry) { create(:notification_template, label: 'thank_you_form_entry', subject_template: '{{subject}}', body_template: "{{body}}") }
+    let(:notification_thank_you_donation) { create(:notification, community: community, activist: activist, notification_template: template_donation, template_vars: { name: 'lorem2', subject: 'Thank you for your donation', body: 'Thanks for your donation for the Mobi 01' }, notification_type: 'auto_fire') }
+    let(:notification_form_entry) { create(:notification, community: community, email: 'jonhdoe@example.org', notification_template: template_form_entry, template_vars: { name: 'John Doe', subject: 'Thank you for supporting Mobi 01', body: 'Hello welcome' }, notification_type: 'auto_fire') }
+
+    context 'send auto_fire of donation' do
+      let(:mail) { NotificationMailer.notify(notification_thank_you_donation) }
+
+      it 'should be a auto_fire' do
+        expect(mail.subject).to eq("Thank you for your donation")
+        expect(mail.to).to eq(["activist@gmail.com"])
+        expect(mail.body.encoded).to include("Thanks for your donation for the Mobi 01")
+        expect(mail['X-SMTPAPI'].present?).to eq(true)
+      end
+    end
+
+    context 'send auto_fire of form_entry' do
+      let(:mail) { NotificationMailer.notify(notification_form_entry) }
+
+      it 'should be a auto_fire' do
+        expect(mail.subject).to eq("Thank you for supporting Mobi 01")
+        expect(mail.to).to eq(["jonhdoe@example.org"])
+        expect(mail.body.encoded).to include("Hello welcome")
+        expect(mail['X-SMTPAPI'].present?).to eq(true)
+      end
+    end
+  end
 end
