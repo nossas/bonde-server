2a80a0c396c275e54e24ed19c859cc1c718e1a0e
Lucas Pirola <lucaspirola@gmail.com>
2019-01-24 10:04:19 -0200

    chore(activists): Update query to build activist_actions;

--- db/migrate/20170208151800_create_activist_actions_view.rb
+++ db/migrate/20170208151800_create_activist_actions_view.rb
@@ -2,53 +2,67 @@ class CreateActivistActionsView < ActiveRecord::Migration
   def up
     execute %Q{
 create or replace view activist_actions as
-select *
-    from (
-    (select
-        'form_entries' as action,
-        w.id as widget_id,
-        m.id as mobilization_id,
-        m.community_id as community_id,
-        fe.activist_id as activist_id,
-        fe.created_at as action_created_date,
-        a.created_at as activist_created_at,
-        a.email as activist_email
+select t.action,
+        t.widget_id,
+        t.mobilization_id,
+        t.community_id,
+        t.activist_id,
+        t.action_created_date,
+        t.activist_created_at,
+        t.activist_email,
+        t.first_action
+from
+    (select 'form_entries'::text as action,
+            w.id as widget_id,
+            m.id as mobilization_id,
+            m.community_id,
+            fe.activist_id,
+            fe.created_at as action_created_date,
+            a.created_at as activist_created_at,
+            a.email as activist_email,
+            case
+                when a.created_at::date = fe.created_at::date then 'sim'::text
+                else 'não'::text
+            end as first_action
     from form_entries fe
-        join activists a on a.id = fe.activist_id
-        join widgets w on w.id = fe.widget_id
-        join blocks b on b.id = w.block_id
-        join mobilizations m on m.id = b.mobilization_id)
-    union all
-    (select
-        'activist_pressures' as action,
-        w.id as widget_id,
-        m.id as mobilization_id,
-        m.community_id as community_id,
-        ap.activist_id as activist_id,
-        ap.created_at as action_created_date,
-        a.created_at as activist_created_at,
-        a.email as activist_email
+    join activists a on a.id = fe.activist_id
+    join widgets w on w.id = fe.widget_id
+    join blocks b on b.id = w.block_id
+    join mobilizations m on m.id = b.mobilization_id
+    union all select 'activist_pressures'::text as action,
+                    w.id as widget_id,
+                    m.id as mobilization_id,
+                    m.community_id,
+                    ap.activist_id,
+                    ap.created_at as action_created_date,
+                    a.created_at as activist_created_at,
+                    a.email as activist_email,
+                    case
+                        when a.created_at = ap.created_at then 'sim'::text
+                        else 'não'::text
+                    end as first_action
     from activist_pressures ap
-        join activists a on a.id = ap.activist_id
-        join widgets w on w.id = ap.widget_id
-        join blocks b on b.id = w.block_id
-        join mobilizations m on m.id = b.mobilization_id)
-    union all
-    (select
-        'donation' as action,
-        w.id as widget_id,
-        m.id as mobilization_id,
-        m.community_id as community_id,
-        d.activist_id as activist_id,
-        d.created_at as action_created_date,
-        a.created_at as activist_created_at,
-        a.email as activist_email
+    join activists a on a.id = ap.activist_id
+    join widgets w on w.id = ap.widget_id
+    join blocks b on b.id = w.block_id
+    join mobilizations m on m.id = b.mobilization_id
+    union all select 'donation'::text as action,
+                    w.id as widget_id,
+                    m.id as mobilization_id,
+                    m.community_id,
+                    d.activist_id,
+                    d.created_at as action_created_date,
+                    a.created_at as activist_created_at,
+                    a.email as activist_email,
+                    case
+                        when a.created_at = d.created_at then 'sim'::text
+                        else 'não'::text
+                    end as first_action
     from donations d
-        join activists a on a.id = d.activist_id
-        join widgets w on w.id = d.widget_id
-        join blocks b on b.id = w.block_id
-        join mobilizations m on m.id = b.mobilization_id)
-    ) as t;
+    join activists a on a.id = d.activist_id
+    join widgets w on w.id = d.widget_id
+    join blocks b on b.id = w.block_id
+    join mobilizations m on m.id = b.mobilization_id) t;
 }
   end
 
