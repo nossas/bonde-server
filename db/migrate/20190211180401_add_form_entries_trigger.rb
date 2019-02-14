5bea97a635626f99a114d667589ae34d5e6716a7
Igor dos Santos <igr.exe@gmail.com>
2019-02-14 20:20:14 -0200

    execute form_entry trigger only widget of mapa

--- db/migrate/20190211180401_add_form_entries_trigger.rb
+++ db/migrate/20190211180401_add_form_entries_trigger.rb
@@ -16,11 +16,18 @@ class AddFormEntriesTrigger < ActiveRecord::Migration
         END;
       $$;
 
+      -- Trigger só irá disparar quando houver atualizando em um desses 3 widgets
+      -- ATENÇÃO: A remoção desses widgets interferem no funcionamento correto da
+      -- integração com o Zendesk.
+      --
+      -- MSR: 16850 // Psicologas: 17628 // Advogadas: 17633
       CREATE TRIGGER watched_create_form_entries_trigger AFTER
       INSERT
       OR
       UPDATE ON public.form_entries
-      FOR EACH ROW EXECUTE PROCEDURE public.notify_form_entries_trigger();
+      FOR EACH ROW
+      WHEN (NEW.widget_id in (16850, 17628, 17633))
+      EXECUTE PROCEDURE public.notify_form_entries_trigger();
   }
   end
 
