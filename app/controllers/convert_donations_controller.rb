e0de0bffff44288c7d0c8d6c1ed519dead1a3bdc
Lucas Pirola <lucaspirola@gmail.com>
2019-04-26 14:04:00 -0300

    feature(replay-donation): Add page with sucess message

--- app/controllers/convert_donations_controller.rb
+++ app/controllers/convert_donations_controller.rb
@@ -36,25 +36,48 @@ class ConvertDonationsController < ApplicationController
     widget = catch_widget
     amount = params[:amount]
 
-    donation = Donation.find_by_email(email).
-      where('donations.transaction_status =  ?', 'paid').last
+    valid_donation = Donation.where('donations.email = ? and donations.transaction_status =  ? and donations.widget_id = ? and donations.created_at::timestamp < (now() - interval \'24 hour\')', email, 'paid', widget.id).last
 
-    if donation.present?
-      new_donation = Donation.new(donation.attributes)
-      new_donation.id = nil
-      new_donation.transaction_id = nil
-      new_donation.widget_id = widget.id
-      new_donation.amount = amount if amount.present?
-      new_donation.converted_from = donation.id
-      new_donation.save
+    if valid_donation.present?
+      valid_donation.converted_from = valid_donation.id
+      valid_donation.id = nil
+      valid_donation.transaction_id = nil
+      valid_donation.transaction_status = nil
+      valid_donation.card_hash = nil
+      valid_donation.created_at = nil
+      valid_donation.updated_at = nil
+      valid_donation.widget_id = widget.id
+      valid_donation.amount = amount if amount.present?
+      valid_donation.mailchimp_syncronization_at = nil
+      valid_donation.mailchimp_syncronization_error_reason = nil
+      valid_donation.skip = true
+      
+      @donation = Donation.create(valid_donation.attributes)
+      @donation.checkout_data = valid_donation[:customer]
+      @donation.cached_community_id = @donation.try(:mobilization).try(:community_id)
 
-      return render json: new_donation
+      if @donation.save!
+        address = find_or_create_address(valid_donation['activist_id'])
+        donation_service = DonationService.run(@donation, address)
+
+        if donation_service == 'refused'
+          render json: { transaction_status: donation_service }, status: :unprocessable_entity
+        else
+          render 'replay' 
+        end
+      else
+        render json: @donation.errors, status: :unprocessable_entity
+      end
     else
       raise ActiveRecord::RecordNotFound
     end
   end
 
   private
+
+  def find_or_create_address(activist_id)
+    @donation.activist.addresses.find(activist_id)
+  end
 
   def catch_widget
     Widget.find params[:widget_id]
