b0671f02c30c45cf1fc23b092576d2d84f673782
Lucas Pirola <lucaspirola@gmail.com>
2019-08-15 14:58:05 -0300

    feature(devops): Upgrade ruby version

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,7 +2,7 @@
 
 class AvatarUploader < CarrierWave::Uploader::Base
 
-  include CarrierWave::RMagick
+  # include CarrierWave::RMagick
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
