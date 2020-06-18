53e186fa90b343f55850e4931451ad8b659bc33f
Lucas Pirola <lucaspirola@gmail.com>
2020-06-18 13:00:31 -0300

    feat(campaign): Add condition to count pressure from multiple widgets

--- app/serializers/activist_pressure_serializer.rb
+++ app/serializers/activist_pressure_serializer.rb
@@ -2,6 +2,10 @@ class ActivistPressureSerializer < ActiveModel::Serializer
   attributes :id, :activist_id, :widget_id, :created_at, :updated_at, :count
 
   def count
-    ActivistPressure.where(widget_id: object.widget_id).count
+    if object.widget_id.to_s.eql?("62189")
+      ActivistPressure.where(widget_id: object.widget_id).count + ActivistPressure.where(widget_id: 62347).count
+    else
+      ActivistPressure.where(widget_id: object.widget_id).count
+    end
   end
 end
